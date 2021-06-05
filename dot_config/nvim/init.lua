9ee373bab862b3436c47f84571cfd1e3953edccb
ylw <cha.cascio@gmail.com>
2021-06-05 21:55:19 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -55,7 +55,8 @@ api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', {noremap = t
 require 'telescope'.setup{
   defaults = {
     preview_cutoff = 0,
-    results_width = 0.5
+    results_width = 0.5,
+    width = 1
   }
 }
 
