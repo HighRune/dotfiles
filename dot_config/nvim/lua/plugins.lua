6264e7e4481cfcd5305227ad0bd1df860ede5639
ylw <cha.cascio@gmail.com>
2021-06-05 20:29:38 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,6 +3,5 @@ return require('packer').startup(function()
   use {
   'nvim-telescope/telescope.nvim',
   requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {"kyazdani42/nvim-web-devicons"}}
-  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
 }
 end)
