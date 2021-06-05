164d1e968c8a3d22430daff07adfb98757b26dda
ylw <cha.cascio@gmail.com>
2021-06-05 20:01:18 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -2,6 +2,6 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use {
   'nvim-telescope/telescope.nvim',
-  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {"kyazdani42/nvim-web-devicons"}}
 }
 end)
