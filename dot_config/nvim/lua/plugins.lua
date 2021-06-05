67ae7daf5d108420b22bc188311eea7904092737
ylw <cha.cascio@gmail.com>
2021-06-05 18:56:28 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,7 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+  use {
+  'nvim-telescope/telescope.nvim',
+  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+}
 end)
