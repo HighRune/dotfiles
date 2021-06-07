9f1d6f8a579c6af685ebb661d8433ff2d88e55fa
ylw <cha.cascio@gmail.com>
2021-06-07 19:34:54 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -87,4 +87,5 @@ require'compe'.setup({
       nvim_lsp = true,
     },
   })
-api.nvim_set_keymap('i', '<CR>', 'compe#complete()', {noremap = true, silent = true})
+-- api.nvim_set_keymap('i', '<expr><CR>', 'compe#complete()', {noremap = true, silent = true})
+api.nvim_set_keymap("i", "<CR> ", "compe#complete()", {noremap = true, silent = true, expr = true})
