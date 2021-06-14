6260234b241d1e3383bfbda58a9768ba5025d528
ylw <cha.cascio@gmail.com>
2021-06-14 19:51:30 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,9 +1,6 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  use {
-  'nvim-telescope/telescope.nvim',
-  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {"kyazdani42/nvim-web-devicons"}}
-}
+  use { 'camspiers/snap' }
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
     use {"romgrk/barbar.nvim"}
     use {
