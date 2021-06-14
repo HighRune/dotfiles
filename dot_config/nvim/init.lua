24da3b66f3d0cba9baf1f1a779440bf2f3295c81
ylw <cha.cascio@gmail.com>
2021-06-14 20:34:44 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -188,17 +188,11 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 
+-- Snap
+
 local snap = require'snap'
 
-local limit = snap.get'consumer.limit'
-local producer_vimgrep = snap.get'producer.ripgrep.vimgrep'
-local producer_buffer = snap.get'producer.vim.buffer'
-local producer_oldfile = snap.get'producer.vim.oldfile'
-local select_file = snap.get'select.file'
-local select_vimgrep = snap.get'select.vimgrep'
-local preview_file = snap.get'preview.file'
-local preview_vimgrep = snap.get'preview.vimgrep'
-
+-- Find Files
 snap.register.map({'n'}, {'<Leader><Leader>'}, function ()
   snap.run({
   producer = snap.get'consumer.fzf'(snap.get'producer.ripgrep.file'),
@@ -208,42 +202,32 @@ snap.register.map({'n'}, {'<Leader><Leader>'}, function ()
   })
 end)
 
-snap.register.map({'n'}, {'<Leader>ff'}, function ()
+-- Live Ripgrep
+snap.register.map({'n'}, {'<Leader>s'}, function ()
+snap.run {
+  producer = snap.get'producer.ripgrep.vimgrep',
+  select = snap.get'select.vimgrep'.select,
+  multiselect = snap.get'select.vimgrep'.multiselect,
+  views = {snap.get'preview.vimgrep'}
+}
+end)
+
+-- Find Buffers
+snap.register.map({'n'}, {'<Leader>b'}, function ()
   snap.run({
-    prompt = 'Grep',
-    producer = limit(10000, producer_vimgrep),
-    select = select_vimgrep.select,
-    multiselect = select_vimgrep.multiselect,
-    views = {preview_vimgrep}
+  producer = snap.get'consumer.fzy'(snap.get'producer.vim.buffer'),
+  select = snap.get'select.file'.select,
+  multiselect = snap.get'select.file'.multiselect,
+  views = {snap.get'preview.file'}
   })
 end)
 
-snap.register.map({'n'}, {'<Leader>fb'}, function ()
+-- Find Git Files
+snap.register.map({'n'}, {'<Leader>g'}, function ()
   snap.run({
-    prompt = 'Buffers',
-    producer = fzy(producer_buffer),
-    select = select_file.select,
-    multiselect = select_file.multiselect,
-    views = {preview_file}
-  })
-end)
-
-snap.register.map({'n'}, {'<Leader>fo'}, function ()
-  snap.run({
-    prompt = 'Oldfiles',
-    producer = fzy(producer_oldfile),
-    select = select_file.select,
-    multiselect = select_file.multiselect,
-    views = {preview_file}
-  })
-end)
-
-snap.register.map({'n'}, {'<Leader>m'}, function ()
-  snap.run({
-    prompt = 'Grep',
-    producer = limit(10000, producer_vimgrep),
-    select = select_vimgrep.select,
-    multiselect = select_vimgrep.multiselect,
-    initial_filter = vim.fn.expand('<cword>')
+producer = snap.get'consumer.fzy'(snap.get'producer.git.file'),
+  select = snap.get'select.file'.select,
+  multiselect = snap.get'select.file'.multiselect,
+  views = {snap.get'preview.file'}
   })
 end)
