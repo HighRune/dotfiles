a65304cb747a0c6def3ce56febc1649b50c46f46
ylw <cha.cascio@gmail.com>
2021-06-24 22:28:53 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,22 +1,27 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-    use {"kyazdani42/nvim-web-devicons"}
-    use { 'camspiers/snap'}
-    use {"romgrk/barbar.nvim"}
-    use {
-  'lewis6991/gitsigns.nvim',
-  requires = {
-    'nvim-lua/plenary.nvim'
-  },
-  config = function()
-    require('gitsigns').setup()
-  end
-  
-}
-use {"neovim/nvim-lspconfig"}
-use {"kabouzeid/nvim-lspinstall"}
-use {"glepnir/lspsaga.nvim"}
-use {"hrsh7th/nvim-compe"}
-use 'folke/tokyonight.nvim'
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+  use {"kyazdani42/nvim-web-devicons"}
+  use { 'camspiers/snap'}
+  use {"romgrk/barbar.nvim"}
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+
+  }
+  use {"neovim/nvim-lspconfig"}
+  use {"kabouzeid/nvim-lspinstall"}
+  use {"glepnir/lspsaga.nvim"}
+  use {"hrsh7th/nvim-compe"}
+  use 'folke/tokyonight.nvim'
+  use {
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    config = function() require'my_statusline' end
+  }
 end)
