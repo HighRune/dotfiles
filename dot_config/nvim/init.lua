b163ebaac34ec5a3787bb5e1d9c839b0f567ffc2
fuel <cha.cascio@gmail.com>
2021-08-09 18:52:59 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -219,7 +219,7 @@ require'fzf-lua'.setup {
   preview_wrap        = 'wrap',       -- wrap|nowrap
   preview_opts        = 'nohidden',     -- hidden|nohidden
   preview_vertical    = 'down:70%',     -- up|down:size
-  preview_horizontal  = 'right:100%',    -- right|left:size
+  preview_horizontal  = 'right:90%',    -- right|left:size
   preview_layout      = 'horizontal',         -- horizontal|vertical|flex
 }
 
