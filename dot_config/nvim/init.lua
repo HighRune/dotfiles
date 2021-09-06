fcd51e6030be10c27da3000e9369348cdb3c1599
fuel <cha.cascio@gmail.com>
2021-09-06 21:27:32 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -170,7 +170,7 @@ require'fzf-lua'.setup {
     win_row          = 1,            -- window row position (0=top, 1=bottom)
     win_col          = 1,            -- window col position (0=left, 1=right)
     -- win_border       = false
-    win_border       = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+    win_border       = { '', '', '', '', '', '', '', '' },
     hl_border        = 'FloatBorder',   -- window border color
   },
   preview_border      = 'noborder',       -- border|noborder
