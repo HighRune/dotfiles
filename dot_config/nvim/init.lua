e158653de44f8363e4a8eb683f9264e78fa0394e
fuel <cha.cascio@gmail.com>
2021-09-29 21:22:42 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -217,9 +217,9 @@ require'fzf-lua'.setup {
 require('gitsigns').setup()
 require'colorizer'.setup()
 
--- o.list = true
--- o.listchars:append("space:⋅")
--- o.listchars:append("eol:↴")
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
