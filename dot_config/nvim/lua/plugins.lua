ee8985de35514e026fe9da57c59539bd2769d6a6
fuel <cha.cascio@gmail.com>
2021-09-29 20:56:33 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -17,7 +17,7 @@ return require('packer').startup(function()
     }
   }
   use {"romgrk/barbar.nvim"}
-  -- use 'folke/tokyonight.nvim'
+  use 'folke/tokyonight.nvim'
   use {"neovim/nvim-lspconfig"}
   use {"kabouzeid/nvim-lspinstall"}
   -- use {"glepnir/lspsaga.nvim"}
