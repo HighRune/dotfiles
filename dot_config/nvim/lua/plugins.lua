c75270f8a747232771f1353e86b43de4a8e2c351
fuel <cha.cascio@gmail.com>
2021-10-02 21:40:25 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function()
   use 'glepnir/lspsaga.nvim'
   use 'sbdchd/neoformat'
   use 'tommcdo/vim-lion'
+  use 'tpope/vim-commentary'
   -- use {'glepnir/galaxyline.nvim', config = "require('config.galaxyline').post()"}
   use 'windwp/windline.nvim'
   use { 'ms-jpq/coq_nvim', branch = 'coq'}
