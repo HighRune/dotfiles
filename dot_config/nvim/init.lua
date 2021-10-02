e1605cd968d967b520d51752401b7ee2bdc812d7
fuel <cha.cascio@gmail.com>
2021-10-02 20:20:22 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,7 +12,8 @@ cmd[[colorscheme tokyonight]]
 -- o.scroll=5
 g.mapleader = " "
 o.mouse="a"                         -- Enables mouse support
-o.ruler=true
+-- o.cursorline=true
+o.cursorcolumn=true
 o.scrolloff=999                     -- Minimal number of screen lines to keep above and below the cursor
 o.foldenable = false                -- All folds are open
 o.number=true                       -- Print the line number in front of each line
