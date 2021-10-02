10f4a6e7564fb288e1789c34eba154bd873010b4
fuel <cha.cascio@gmail.com>
2021-10-02 10:28:29 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -60,6 +60,17 @@ api.nvim_set_keymap('n', '<leader>f', ':Neoformat eslint_d<CR>', { noremap = tru
 require('plugins')
 
 -- Telescope
+require('telescope').setup({
+  defaults = {
+    layout_config = {
+      vertical = { width = 0.5 }
+      -- other layout configuration here
+    },
+    -- other defaults configuration here
+  },
+  -- other configuration values here
+})
+
 api.nvim_set_keymap('n', '<leader><leader>', "<cmd>Telescope find_files theme=dropdown<cr>", {noremap = true, silent = true})
 api.nvim_set_keymap('n', '<leader>s', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
 api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
