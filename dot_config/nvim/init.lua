e465c970863877f4ea445800c8878857788f94ed
fuel <cha.cascio@gmail.com>
2021-10-02 10:28:57 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -63,7 +63,7 @@ require('plugins')
 require('telescope').setup({
   defaults = {
     layout_config = {
-      vertical = { width = 0.5 }
+      vertical = { width = 0.9 }
       -- other layout configuration here
     },
     -- other defaults configuration here
