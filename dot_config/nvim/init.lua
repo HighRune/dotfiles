549fd24a850a7524b2eebc47bbf933783a0884cd
fuel <cha.cascio@gmail.com>
2021-10-02 10:09:56 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,7 +2,7 @@ local api = vim.api
 local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
 local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
-local o = vim.o  -- to set options
+local o = vim.o      -- to set options
 
 -- ____ GENERAL
 
@@ -11,15 +11,15 @@ g.tokyonight_transparent = true
 cmd[[colorscheme tokyonight]]
 -- o.scroll=5
 g.mapleader = " "
-o.mouse="a"                                   -- Enables mouse support
-o.scrolloff=999                              -- Minimal number of screen lines to keep above and below the cursor
-o.foldenable = false                              -- All folds are open
-o.number=true         -- Print the line number in front of each line
+o.mouse="a"                         -- Enables mouse support
+o.scrolloff=999                     -- Minimal number of screen lines to keep above and below the cursor
+o.foldenable = false                -- All folds are open
+o.number=true                       -- Print the line number in front of each line
 o.virtualedit="all"
-o.completeopt="menuone,noinsert"              -- Options for Insert mode completion
-o.clipboard = "unnamedplus"                    -- Have the clipboard be the same as my regular clipboard
-o.updatetime=100                            -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience
-o.hidden=true                                    -- Buffer becomes hidden when it is abandoned
+o.completeopt="menuone,noinsert"    -- Options for Insert mode completion
+o.clipboard = "unnamedplus"         -- Have the clipboard be the same as my regular clipboard
+o.updatetime=100                    -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience
+o.hidden=true                       -- Buffer becomes hidden when it is abandoned
 o.swapfile=false
 
 -- autocmd BufEnter * :syntax sync fromstart     -- Fix syntax color
@@ -27,7 +27,7 @@ cmd[[autocmd BufEnter *.vue,*.js,*.ts,*.md :set scroll =4]]
 cmd[[autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path %]]
 cmd[[autocmd BufLeave ~/.config/cheatsheet.md ! chezmoi add ~/.config/cheatsheet.md]]
 cmd[[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]]
-cmd[[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]]
+-- cmd[[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]]
 
 -- ____ UI
 
@@ -45,12 +45,12 @@ o.smartcase = true  -- Override the 'ignorecase' option if the search pattern co
 
 -- ____ INDENTATION
 
-o.expandtab=true                   -- Use the appropriate number of spaces to insert a <Tab>
-o.smartindent=true                 -- Do smart autoindenting when starting a new line
-o.copyindent=true                  -- Copy the structure of the existing lines indent when autoindenting a new line
-o.softtabstop=2               -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
-o.tabstop=2                   -- Number of spaces that a <Tab> in the file counts for
-o.shiftwidth=2                -- Number of spaces to use for each step of (auto)indent
+o.expandtab=true          -- Use the appropriate number of spaces to insert a <Tab>
+o.smartindent=true        -- Do smart autoindenting when starting a new line
+o.copyindent=true         -- Copy the structure of the existing lines indent when autoindenting a new line
+o.softtabstop=2           -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
+o.tabstop=2               -- Number of spaces that a <Tab> in the file counts for
+o.shiftwidth=2            -- Number of spaces to use for each step of (auto)indent
 
 api.nvim_set_keymap('n', '<C-j>', '2j', { noremap = true, silent = true })
 api.nvim_set_keymap('n', '<C-k>', '2k', { noremap = true, silent = true })
