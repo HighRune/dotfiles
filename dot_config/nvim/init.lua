eb9f227d689c04f79f9dff00b5190056149a924f
fuel <cha.cascio@gmail.com>
2021-10-03 20:12:14 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -268,7 +268,7 @@ vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
-    show_current_context = true,
+    show_end_of_line = true,
 }
 
 -- local saga = require 'lspsaga'
