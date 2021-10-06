3719f3cb015b100142e2f790cf47496fe903a154
fuel <cha.cascio@gmail.com>
2021-10-06 21:01:37 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -15,8 +15,8 @@ return require('packer').startup(function()
   use 'tpope/vim-commentary'
   use 'JoosepAlviste/nvim-ts-context-commentstring'
   use 'tommcdo/vim-lion'
-  -- use 'dstein64/nvim-scrollview'
   use 'nacro90/numb.nvim'
+  use 'Xuyuanp/scrollbar.nvim'
   -- use {'glepnir/galaxyline.nvim', config = "require('config.galaxyline').post()"}
   use 'windwp/windline.nvim'
   use { 'ms-jpq/coq_nvim', branch = 'coq'}
