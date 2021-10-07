31cc152a0d04018f65a6e9cbb62c4199fe9f9406
fuel <cha.cascio@gmail.com>
2021-10-07 09:42:42 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -32,6 +32,7 @@ cmd[[autocmd BufEnter *.vue,*.js,*.ts,*.md :set scroll =5]]
 cmd[[autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path %]]
 cmd[[autocmd BufLeave ~/.config/cheatsheet.md ! chezmoi add ~/.config/cheatsheet.md]]
 cmd[[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]]
+cmd[[autocmd ColorScheme * highlight NormalFloat guibg=NONE]]
 
 -- cmd[[augroup ScrollbarInit
 --   autocmd!
