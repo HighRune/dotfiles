f28c32b4bbe47a7abba4b561ddba38c13d06cdd2
fuel <cha.cascio@gmail.com>
2021-10-07 20:26:32 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -5,6 +5,7 @@ return require('packer').startup(function()
   use 'nvim-treesitter/playground'
   use 'kyazdani42/nvim-web-devicons'
   -- use { 'ibhagwan/fzf-lua', requires = { 'vijaymarupudi/nvim-fzf' } }
+  use 'yamatsum/nvim-cursorline'
   use 'lewis6991/gitsigns.nvim'
   use 'romgrk/barbar.nvim'
   use 'lukas-reineke/indent-blankline.nvim'
