8744ae82ec2cf0def54fad98849f8937e858ed52
fuel <cha.cascio@gmail.com>
2021-10-09 19:28:45 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -233,18 +233,3 @@ api.nvim_set_keymap("n", "<leader>b", "<cmd>Telescope buffers<cr>", opts)
 api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>", opts)
 api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", opts)
 api.nvim_set_keymap("n", "<C-w>", ":BufferClose<CR>", opts)
-
--------------------- AckslD/nvim-revJ.lua
-require("revj").setup({
-	brackets = { first = "([{<", last = ")]}>" }, -- brackets to consider surrounding arguments
-	new_line_before_last_bracket = true, -- add new line between last argument and last bracket (only if no last seperator)
-	add_seperator_for_last_parameter = true, -- if a seperator should be added if not present after last parameter
-	enable_default_keymaps = false, -- enables default keymaps without having to set them below
-	keymaps = {
-		operator = "<leader>J", -- for operator (+motion)
-		line = "<leader>j", -- for formatting current line
-		visual = "<leader>j", -- for formatting visual selection
-	},
-	parameter_mapping = ",", -- specifies what text object selects an arguments (ie a, and i, by default)
-	-- if you're using `vim-textobj-parameter` you can also set this to `vim.g.vim_textobj_parameter_mapping`
-})
