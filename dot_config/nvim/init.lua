ebd41685f295479fb530b0abc4ba6fa468c24ed9
fuel <cha.cascio@gmail.com>
2021-10-11 23:34:02 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -273,17 +273,6 @@ cmd([[
 
 cmd([[
 map <s-right> <Plug>(expand_region_expand)
-map <s-left>(expand_region_shrink)
-let g:expand_region_text_objects = {
-      \ 'iw'  :0,
-      \ 'iW'  :0,
-      \ 'i"'  :0,
-      \ 'i''' :0,
-      \ 'i]'  :1,
-      \ 'ib'  :1,
-      \ 'iB'  :1,
-      \ 'il'  :0,
-      \ 'ip'  :0,
-      \ 'ie'  :0,
-      \ }
+map <s-left> <Plug>(expand_region_shrink)
+let g:expand_region_text_objects = { 'i"':1, 'i''':1, 'i`': 1, 'i]':1, 'i)':1, 'i}':1, 'it':1, 'ii':1, 'a"':1, 'a''':1, 'a`': 1, 'a]':1, 'a)':1, 'a}':1, 'at':1, }
 ]])
