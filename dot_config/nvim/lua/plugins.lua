0589bc0bdb858b91a757274b8ed1cab16d227947
fuel <cha.cascio@gmail.com>
2021-10-13 22:39:04 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,5 +1,5 @@
 return require("packer").startup(function(use)
-	-- use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 	-- use("nvim-lua/plenary.nvim")
 	-- use({
 	-- 	"nvim-treesitter/nvim-treesitter",
