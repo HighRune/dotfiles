4dc53f62593207db036d60a3dc371f324aad549a
fuel <cha.cascio@gmail.com>
2021-10-15 20:44:17 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -129,21 +129,21 @@ o.foldmethod = "expr"
 o.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- -------------------- hrsh7th/nvim-cmp
---   local cmp = require'cmp'
+  local cmp = require'cmp'
 
---   cmp.setup({
---     mapping = {
---       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---       ['<C-f>'] = cmp.mapping.scroll_docs(4),
---       ['<C-Space>'] = cmp.mapping.complete(),
---       ['<C-e>'] = cmp.mapping.close(),
---       ['<CR>'] = cmp.mapping.confirm({ select = true }),
---     },
---     sources = {
---       { name = 'nvim_lsp' },
---       { name = 'buffer' },
---     }
---   })
+  cmp.setup({
+    mapping = {
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'buffer' },
+    }
+  })
 
 -------------------- neovim/nvim-lspconfig
 -- Use an on_attach function to only map the following keys
