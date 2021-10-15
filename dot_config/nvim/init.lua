faa1a34e4b65bc0483d6ce35edaf27b315733a50
fuel <cha.cascio@gmail.com>
2021-10-15 20:49:06 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -179,8 +179,8 @@ local function setup_servers()
 	local servers = require("lspinstall").installed_servers()
 	for _, server in pairs(servers) do
 		require("lspconfig")[server].setup({
-capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 			on_attach = on_attach,
+			capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
 			flags = {
 				debounce_text_changes = 150,
 			},
