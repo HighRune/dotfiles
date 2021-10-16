185fea68209b2e92b24958d1f90e0f8f050f935e
fuel <cha.cascio@gmail.com>
2021-10-16 07:31:12 +0200

    Update dot_config/nvim/lua/config/treesitter.lua

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,55 +1,49 @@
-local M = {}
-
-function M.post()
-	require("nvim-treesitter.configs").setup({
-		context_commentstring = { -- JoosepAlviste/nvim-ts-context-commentstring
-			enable = true,
+require("nvim-treesitter.configs").setup({
+	context_commentstring = { -- JoosepAlviste/nvim-ts-context-commentstring
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+	rainbow = { -- p00f/nvim-ts-rainbow
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
+	ensure_installed = "maintained",
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
 		},
-		autotag = {
-			enable = true,
+	},
+	playground = { -- nvim-treesitter/playground
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
 		},
-		rainbow = { -- p00f/nvim-ts-rainbow
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
-		},
-		ensure_installed = "maintained",
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn",
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
-			},
-		},
-		playground = { -- nvim-treesitter/playground
-			enable = true,
-			disable = {},
-			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-			persist_queries = false, -- Whether the query persists across vim sessions
-			keybindings = {
-				toggle_query_editor = "o",
-				toggle_hl_groups = "i",
-				toggle_injected_languages = "t",
-				toggle_anonymous_nodes = "a",
-				toggle_language_display = "I",
-				focus_language = "f",
-				unfocus_language = "F",
-				update = "R",
-				goto_node = "<cr>",
-				show_help = "?",
-			},
-		},
-	})
-	vim.o.foldmethod = "expr"
-	vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-end
-
-return M
+	},
+})
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
