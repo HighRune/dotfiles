c382dfbf8ae75efd210cc542f75c74f5249dd7e9
fuel <cha.cascio@gmail.com>
2021-10-17 22:11:04 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -206,9 +206,9 @@ api.nvim_set_keymap("n", "<TAB>", ":BufferNext<CR>", opts)
 api.nvim_set_keymap("n", "<S-TAB>", ":BufferPrevious<CR>", opts)
 api.nvim_set_keymap("n", "<C-w>", ":BufferClose<CR>", opts)
 
--- -- -------------------- AndrewRadev/sideways.vim
--- api.nvim_set_keymap("n", "<s-h>", ":SidewaysLeft<cr>", opts)
--- api.nvim_set_keymap("n", "<s-l>", ":SidewaysRight<cr>", opts)
+-- -------------------- AndrewRadev/sideways.vim
+api.nvim_set_keymap("n", "<s-h>", ":SidewaysLeft<cr>", opts)
+api.nvim_set_keymap("n", "<s-l>", ":SidewaysRight<cr>", opts)
 
 -------------------- AndrewRadev/splitjoin.vim
 api.nvim_set_keymap("n", "<c-j>", ":SplitjoinJoin<cr>", opts)
