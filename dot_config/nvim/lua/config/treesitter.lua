136b61d7ad1bf7eab08236cf6b766af7e4559e09
fuel <cha.cascio@gmail.com>
2021-10-17 18:56:15 +0200

    Update dot_config/nvim/lua/config/treesitter.lua

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -4,7 +4,7 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 	},
 	indent = {
-		enable = true,
+		enable = false,
 	},
 	-- JoosepAlviste/nvim-ts-context-commentstring
 	context_commentstring = {
