f42a2f58fbbf302126e4bc3e0be157c6e0c030f2
fuel <cha.cascio@gmail.com>
2021-10-18 19:27:17 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -125,7 +125,7 @@ require("nvim-lsp-installer").on_server_ready(function(server)
 			-- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
 			-- the resolved capabilities of the eslint server ourselves!
 			client.resolved_capabilities.document_formatting = true
-			common_on_attach(client, bufnr)
+			on_attach(client, bufnr)
 		end
 		opts.settings = {
 			format = { enable = true }, -- this will enable formatting
