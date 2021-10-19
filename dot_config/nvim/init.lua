44a5aff91f1cacb1f241019d6d1697b406036fdc
fuel <cha.cascio@gmail.com>
2021-10-19 09:51:06 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -35,7 +35,7 @@ o.updatetime = 100 -- Having longer updatetime (default is 4000 ms = 4 s) leads 
 o.swapfile = false
 o.termguicolors = true -- Enables 24-bit RGB color in the Terminal UI
 o.showmode = false -- Disable message on the last line (Insert, Replace or Visual mode)
-o.formatoptions="cro"
+o.formatoptions = "cro"
 -- o.showtabline=2  -- Always display the line with tab page labels
 o.ignorecase = true -- Ignore case in search patterns
 o.smartcase = true -- Override the 'ignorecase' option if the search pattern contains upper case characters
