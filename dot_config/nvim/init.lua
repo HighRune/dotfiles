4d2a7cf081d2bb9ecce74a650e53ac86382ed006
fuel <cha.cascio@gmail.com>
2021-10-19 09:07:02 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -209,45 +209,45 @@ cmd("let g:neoformat_enabled_lua = ['stylua']")
 
 -- vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]])
 -- api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
---
-local remap = vim.api.nvim_set_keymap
-local npairs = require("nvim-autopairs")
-
-npairs.setup({ map_bs = false })
-
-vim.g.coq_settings = { keymap = { recommended = false } }
-
--- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 
 -------------------- windwp/nvim-autopairs
--- skip it, if you use another global object
-_G.MUtils = {}
+-- local remap = vim.api.nvim_set_keymap
+-- local npairs = require("nvim-autopairs")
 
-MUtils.CR = function()
-	if vim.fn.pumvisible() ~= 0 then
-		if vim.fn.complete_info({ "selected" }).selected ~= -1 then
-			return npairs.esc("<c-y>")
-		else
-			return npairs.esc("<c-e>") .. npairs.autopairs_cr()
-		end
-	else
-		return npairs.autopairs_cr()
-	end
-end
-remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+-- npairs.setup({ map_bs = false })
 
-MUtils.BS = function()
-	if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
-		return npairs.esc("<c-e>") .. npairs.autopairs_bs()
-	else
-		return npairs.autopairs_bs()
-	end
-end
-remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
+-- vim.g.coq_settings = { keymap = { recommended = false } }
+
+-- -- these mappings are coq recommended mappings unrelated to nvim-autopairs
+-- remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+-- remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+-- remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+-- remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+-- -- skip it, if you use another global object
+-- _G.MUtils = {}
+
+-- MUtils.CR = function()
+-- 	if vim.fn.pumvisible() ~= 0 then
+-- 		if vim.fn.complete_info({ "selected" }).selected ~= -1 then
+-- 			return npairs.esc("<c-y>")
+-- 		else
+-- 			return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+-- 		end
+-- 	else
+-- 		return npairs.autopairs_cr()
+-- 	end
+-- end
+-- remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+
+-- MUtils.BS = function()
+-- 	if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
+-- 		return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+-- 	else
+-- 		return npairs.autopairs_bs()
+-- 	end
+-- end
+-- remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
 
 -------------------- lukas-reineke/indent-blankline.nvim
 opt.list = true
