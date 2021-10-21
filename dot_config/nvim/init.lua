895bc8441f9ccd94bbacd879e53cf11ec12e26b3
fuel <cha.cascio@gmail.com>
2021-10-22 00:35:25 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -302,19 +302,6 @@ api.nvim_set_keymap("n", "<C-w>", ":BufferClose<CR>", opts)
 api.nvim_set_keymap("n", "<s-j>", ":SplitjoinJoin<cr>", opts)
 api.nvim_set_keymap("n", "<s-k>", ":SplitjoinSplit<cr>", opts)
 
--- -------------------- bkad/CamelCaseMotion
--- -- Map to |w|, |b| and |e| mappings: >
--- cmd([[
---     map <silent> w <Plug>CamelCaseMotion_w
---     map <silent> b <Plug>CamelCaseMotion_b
---     map <silent> e <Plug>CamelCaseMotion_e
---     map <silent> ge <Plug>CamelCaseMotion_ge
---     sunmap w
---     sunmap b
---     sunmap e
---     sunmap ge
--- ]])
-
 -------------------- terryma/vim-expand-region
 -- cmd([[
 -- map <cr> <Plug>(expand_region_expand)
