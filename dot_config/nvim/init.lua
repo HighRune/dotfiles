33d820f812ec536cbf1f3a93a982c70d404b6876
fuel <cha.cascio@gmail.com>
2021-10-21 14:31:14 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -215,7 +215,7 @@ cmd("let g:neoformat_enabled_lua = ['stylua']")
 cmd([[
 augroup fmt
   autocmd!
-  au BufWritePre * try | undojoin | Neoformat stylua | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+  au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
 augroup END
 ]])
 
