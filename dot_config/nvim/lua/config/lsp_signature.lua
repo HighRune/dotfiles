9e7550511264c91a44c7c3a53d37e8ce68980b84
fuel <cha.cascio@gmail.com>
2021-10-24 10:41:03 +0200

    Add dot_config/nvim/lua/config/lsp_signature.lua

---
+++ dot_config/nvim/lua/config/lsp_signature.lua
@@ -0,0 +1,15 @@
+require("lsp_signature").setup({
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	handler_opts = {
+		border = "single",
+	},
+	doc_lines = 0,
+	hint_prefix = "",
+	hint_scheme = "Comment",
+	tranpancy = 100,
+	floating_window = false,
+	max_width = 80,
+	always_trigger = true,
+	max_height = 15,
+	hint_enable = true,
+})
