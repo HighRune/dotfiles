43cd3cf9295da7c1609a132829c4608bcdd93f8c
fuel <cha.cascio@gmail.com>
2021-10-24 18:09:37 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -52,10 +52,10 @@ return require("packer").startup(function(use)
 			require("numb").setup()
 		end,
 	})
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		config = require("config.indent-blankline").setup(),
-	})
+	-- use({
+	-- 	"lukas-reineke/indent-blankline.nvim",
+	-- 	config = require("config.indent-blankline").setup(),
+	-- })
 	use("romgrk/barbar.nvim")
 	use("folke/tokyonight.nvim")
 	use({ "ms-jpq/coq.artifacts", branch = "artifacts" })
