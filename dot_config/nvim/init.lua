18c5572330a32d6d46dd4f844d3919486356a1c9
fuel <cha.cascio@gmail.com>
2021-10-24 10:39:09 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -105,23 +105,6 @@ g.coq_settings = {
 	},
 }
 
--------------------- ray-x/lsp_signature.nvim
-require("lsp_signature").setup({
-	bind = true, -- This is mandatory, otherwise border config won't get registered.
-	handler_opts = {
-		border = "single",
-	},
-	doc_lines = 0,
-	hint_prefix = "",
-	hint_scheme = "Comment",
-	tranpancy = 100,
-	floating_window = false,
-	max_width = 80,
-	always_trigger = true,
-	max_height = 15,
-	hint_enable = true,
-})
-
 -------------------- sbdchd/neoformat
 cmd("let g:neoformat_enabled_lua = ['stylua']")
 -- cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
