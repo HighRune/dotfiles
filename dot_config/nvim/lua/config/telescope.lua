d5bc5ef92dd34bd5044ceb75b0544e9101951216
fuel <cha.cascio@gmail.com>
2021-10-24 11:43:11 +0200

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,15 +1,19 @@
-require("telescope").setup({
-	defaults = {
-		layout_strategy = "vertical",
-		layout_config = {
-			preview_cutoff = 0,
-			height = 0.999,
-			width = 0.999,
-		},
-		mappings = {
-			i = {
-				["<esc>"] = require("telescope.actions").close,
+local function setup()
+	require("telescope").setup({
+		defaults = {
+			layout_strategy = "vertical",
+			layout_config = {
+				preview_cutoff = 0,
+				height = 0.999,
+				width = 0.999,
+			},
+			mappings = {
+				i = {
+					["<esc>"] = require("telescope.actions").close,
+				},
 			},
 		},
-	},
-})
+	})
+end
+
+return { setup = setup }
