fb5a1ac1871f4930ad449620ea261543d62e4039
fuel <cha.cascio@gmail.com>
2021-10-24 10:04:27 +0200

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,18 +1,18 @@
-	require("telescope").setup({
-		defaults = {
-			layout_strategy = "vertical",
-			layout_config = {
-				preview_cutoff = 0,
-				height = 0.999,
-				width = 0.999,
-			},
-			mappings = {
-				i = {
-					["<esc>"] = require("telescope.actions").close,
-				},
-			},
-		},
-	})
-	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", opts)
-	vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>Telescope live_grep<cr>", opts)
-	vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>Telescope buffers<cr>", opts)
+require("telescope").setup({
+  defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      preview_cutoff = 0,
+      height = 0.999,
+      width = 0.999,
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = require("telescope.actions").close,
+      },
+    },
+  },
+})
+vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>Telescope live_grep<cr>", opts)
+vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>Telescope buffers<cr>", opts)
