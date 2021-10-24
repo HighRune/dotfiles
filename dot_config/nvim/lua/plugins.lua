0e6117c6e1e71f7dcb55388a7168bca1a957e7ce
fuel <cha.cascio@gmail.com>
2021-10-24 10:29:42 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -11,7 +11,7 @@ return require("packer").startup(function(use)
 	})
 	use({
 		"nvim-telescope/telescope.nvim",
-		config = require("config.telescope").setup(),
+		config = function() require("config.telescope") end,,
 	})
 
 	use("nvim-treesitter/nvim-treesitter-textobjects")
