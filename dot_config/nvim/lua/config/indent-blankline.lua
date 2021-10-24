bcb7ac2f8915a8d6948f4caab8dc6a40c5f86388
fuel <cha.cascio@gmail.com>
2021-10-24 11:21:01 +0200

    Update dot_config/nvim/lua/config/indent-blankline.lua

--- dot_config/nvim/lua/config/indent-blankline.lua
+++ dot_config/nvim/lua/config/indent-blankline.lua
@@ -1,5 +1,5 @@
-opt.list = true
-opt.listchars:append("eol:↴")
+vim.opt.list = true
+vim.opt.listchars:append("eol:↴")
 require("indent_blankline").setup({
 	show_end_of_line = true,
 	buftype_exclude = { "terminal", "nofile" },
