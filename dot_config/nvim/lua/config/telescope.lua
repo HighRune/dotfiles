94e2f24daf6a9d69cd8631cbb9c40402570b6bd4
fuel <cha.cascio@gmail.com>
2021-10-24 10:03:43 +0200

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,4 +1,3 @@
-local function setup()
 	require("telescope").setup({
 		defaults = {
 			layout_strategy = "vertical",
@@ -17,6 +16,3 @@ local function setup()
 	vim.api.nvim_set_keymap("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", opts)
 	vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>Telescope live_grep<cr>", opts)
 	vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>Telescope buffers<cr>", opts)
-end
-
-return { setup = setup }
