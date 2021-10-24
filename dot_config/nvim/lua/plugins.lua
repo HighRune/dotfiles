4b9e12fd9d24065dc5ce81d2a442439ab5178791
fuel <cha.cascio@gmail.com>
2021-10-24 19:03:07 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -23,11 +23,11 @@ return require("packer").startup(function(use)
 	})
 	use({
 		"ray-x/lsp_signature.nvim",
-		-- config = require("config.lsp_signature").setup(),
+		config = require("config.lsp_signature").setup(),
 	})
 	use({
 		"is0n/fm-nvim",
-		-- config = require("config.fm-nvim").setup(),
+		config = require("config.fm-nvim").setup(),
 	})
 	-- use("nvim-treesitter/nvim-treesitter-textobjects")
 	-- use("RRethy/nvim-treesitter-textsubjects")
