29a8c84a8f6eef7b98b776378f5d18e36351245d
fuel <cha.cascio@gmail.com>
2021-10-24 10:46:00 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -190,26 +190,6 @@ api.nvim_set_keymap("n", "<s-k>", ":SplitjoinSplit<cr>", opts)
 cmd([[let g:vim_search_pulse_mode = 'pattern']])
 cmd([[let g:vim_search_pulse_duration = 200]])
 
--------------------- is0n/fm-nvim
-require("fm-nvim").setup({
-	border = "none",
-	height = 1,
-	width = 1,
-	-- Command used to open files: 'tabedit'; 'split'; 'pedit'; ...
-	edit_cmd = "edit",
-	-- Terminal commands used w/ file manager
-	cmds = {
-		vifm_cmd = "vifm",
-	},
-	-- Mappings used inside the floating window
-	mappings = {
-		vert_split = "<C-v>",
-		horz_split = "<C-h>",
-		tabedit = "<C-h>",
-		edit = "<C-e>",
-	},
-})
-
 api.nvim_set_keymap("n", "<leader>n", ":Vifm<cr>", opts)
 
 -------------------- chaoren/vim-wordmotion
