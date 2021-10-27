53cbf8524b9892d9bb43fc8961dd0bc463ae1cd3
fuel <cha.cascio@gmail.com>
2021-10-28 00:06:22 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -142,8 +142,8 @@ api.nvim_set_keymap("n", "<s-l>", ":SidewaysRight<cr>", opts)
 
 -------------------- svermeulen/vim-yoink
 cmd([[
-nmap <c-n> <plug>(YoinkPostPasteSwapBack)
-nmap <c-p> <plug>(YoinkPostPasteSwapForward)
+nmap <cr> <plug>(YoinkPostPasteSwapBack)
+nmap <s-cr> <plug>(YoinkPostPasteSwapForward)
 
 nmap p <plug>(YoinkPaste_p)
 nmap P <plug>(YoinkPaste_P)
@@ -151,4 +151,9 @@ nmap P <plug>(YoinkPaste_P)
 " Also replace the default gp with yoink paste so we can toggle paste in this case too
 nmap gp <plug>(YoinkPaste_gp)
 nmap gP <plug>(YoinkPaste_gP)
+
+let g:yoinkMaxItems = 5
+let g:yoinkAutoFormatPaste = 1
+let g:yoinkSwapClampAtEnds = 0
+let g:yoinkSyncSystemClipboardOnFocus = 0
 ]])
