8485c1e3e4859dac71d27d7ab61870f112a358fa
fuel <cha.cascio@gmail.com>
2021-10-31 15:31:41 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -59,10 +59,10 @@ cmd([[autocmd BufLeave ~/.config/cheatsheet.md :silent ! ! chezmoi add ~/.config
 cmd([[autocmd BufWinEnter,BufRead,BufNewFile * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
 
 api.nvim_set_keymap("n", "<C-l>", ":noh<cr>", opts)
--- api.nvim_set_keymap("n", "<PageUp>", "6k", opts)
--- api.nvim_set_keymap("n", "<PageDown>", "6j", opts)
--- api.nvim_set_keymap("n", "<c-u>", "5k", opts)
--- api.nvim_set_keymap("n", "<c-d>", "5j", opts)
+api.nvim_set_keymap("n", "<PageUp>", "5k", opts)
+api.nvim_set_keymap("n", "<PageDown>", "5j", opts)
+api.nvim_set_keymap("n", "<c-u>", "5k", opts)
+api.nvim_set_keymap("n", "<c-d>", "5j", opts)
 api.nvim_set_keymap("", "Q", "<Nop>", opts)
 
 -- cmd([[autocmd BufEnter * :syntax sync fromstart]])     -- Fix syntax color
@@ -154,3 +154,26 @@ let g:yoinkAutoFormatPaste = 1
 let g:yoinkSwapClampAtEnds = 0
 let g:yoinkSyncSystemClipboardOnFocus = 0
 ]])
+
+require("neoscroll").setup({
+	easing_function = "quadratic", -- Default easing function
+	-- Set any other options as needed
+})
+
+local t = {}
+-- Syntax: t[keys] = {function, {function arguments}}
+-- Use the "sine" easing function
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "350", [['sine']] } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350", [['sine']] } }
+-- Use the "circular" easing function
+t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+-- Pass "nil" to disable the easing animation (constant scrolling speed)
+t["<C-y>"] = { "scroll", { "-0.10", "false", "100", nil } }
+t["<C-e>"] = { "scroll", { "0.10", "false", "100", nil } }
+-- When no easing function is provided the default easing function (in this case "quadratic") will be used
+t["zt"] = { "zt", { "300" } }
+t["zz"] = { "zz", { "300" } }
+t["zb"] = { "zb", { "300" } }
+
+require("neoscroll.config").set_mappings(t)
