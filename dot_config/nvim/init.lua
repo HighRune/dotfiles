2143872cc0a39dcf791f355075f9c611f7b18381
fuel <cha.cascio@gmail.com>
2021-11-01 14:49:20 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -204,5 +204,31 @@ MUtils.BS = function()
 end
 remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
 
-vim.api.nvim_set_keymap("i", "<Tab>", "<Plug>(Tabout)", { expr = true })
-vim.api.nvim_set_keymap("i", "<S-Tab>", "<Plug>(TaboutBack)", { expr = true })
+require("tabout").setup({
+	tabkey = "",
+	backwards_tabkey = "",
+})
+
+local function replace_keycodes(str)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+function _G.tab_binding()
+	if vim.fn.pumvisible() ~= 0 then
+		return replace_keycodes("<C-n>")
+	elseif vim.fn["vsnip#available"](1) ~= 0 then
+		return replace_keycodes("<Plug>(vsnip-expand-or-jump)")
+	else
+		return replace_keycodes("<Plug>(Tabout)")
+	end
+end
+
+function _G.s_tab_binding()
+	if vim.fn.pumvisible() ~= 0 then
+		return replace_keycodes("<C-p>")
+	elseif vim.fn["vsnip#jumpable"](-1) ~= 0 then
+		return replace_keycodes("<Plug>(vsnip-jump-prev)")
+	else
+		return replace_keycodes("<Plug>(TaboutBack)")
+	end
+end
