9f7616b29c9b1060cd7307af30f8c2aa857cbcbb
fuel <cha.cascio@gmail.com>
2021-11-04 20:48:41 +0100

    Update dot_config/nvim/lua/config/indent-blankline.lua

--- dot_config/nvim/lua/config/indent-blankline.lua
+++ dot_config/nvim/lua/config/indent-blankline.lua
@@ -1,7 +1,5 @@
 local function setup()
 	require("indent_blankline").setup({
-		indent_blankline_enabled = true,
-		indent_blankline_show_first_indent_level = false,
 		show_end_of_line = true,
 		filetype_exclude = { "help" },
 		buftype_exclude = { "terminal", "nofile" },
