d3b179036dfe026db167b34c492888b2039ab9ce
fuel <cha.cascio@gmail.com>
2021-11-06 18:49:20 +0100

    Update dot_config/nvim/lua/config/coq.lua

--- dot_config/nvim/lua/config/coq.lua
+++ dot_config/nvim/lua/config/coq.lua
@@ -14,17 +14,8 @@ local function setup()
 		keymap = {
 			jump_to_mark = "<c-cr>",
 			bigger_preview = "",
-			recommended = false,
 		},
 	}
-	vim.cmd([[
-ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>"
-ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
-ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
-ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
-ino <silent><expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
-	]])
 end
 
 return { setup = setup }
