2930841c8b52cfaeed354c60b4b843bb61860498
fuel <cha.cascio@gmail.com>
2021-11-06 17:25:34 +0100

    Update dot_config/nvim/lua/config/coq.lua

--- dot_config/nvim/lua/config/coq.lua
+++ dot_config/nvim/lua/config/coq.lua
@@ -12,10 +12,25 @@ local function setup()
 			},
 		},
 		keymap = {
-			jump_to_mark = "<c-n>",
-			bigger_preview = "",
+			recommended = false,
+			-- jump_to_mark = "<c-n>",
+			-- bigger_preview = "",
 		},
 	}
+	vim.cmd([[
+inoremap <expr> <C-n> pumvisible() ? "" : "\<C-n>"
+inoremap <expr> <C-j> pumvisible() ? "\<C-n>" : "\<C-j>"
+
+inoremap <expr> <C-p> pumvisible() ? "" : "\<C-p>"
+inoremap <expr> <C-k> pumvisible() ? "\<C-p>" : "\<C-k>"
+  ]])
+	-- vim.cmd([[
+	-- ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>"
+	-- ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+	-- ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+	-- ino <silent><expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+	-- ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
+	-- ]])
 end
 
 return { setup = setup }
