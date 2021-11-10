68dafea9a86a90fae9760b8508c9cb757e338717
fuel <cha.cascio@gmail.com>
2021-11-10 20:37:47 +0100

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -21,32 +21,34 @@ return require("packer").startup(function(use)
 		"nvim-telescope/telescope.nvim",
 		config = require("config.telescope").setup(),
 	})
-	-- use({
-	-- 	"is0n/fm-nvim",
-	-- 	config = "require('config.fm-nvim').setup()",
-	-- })
-	-- use({
-	-- 	"lewis6991/gitsigns.nvim",
-	-- 	config = function()
-	-- 		require("gitsigns").setup()
-	-- 	end,
-	-- })
-	-- use({
-	-- 	"norcalli/nvim-colorizer.lua",
-	-- 	config = function()
-	-- 		require("colorizer").setup()
-	-- 	end,
-	-- })
-	-- use({
-	-- 	"nacro90/numb.nvim",
-	-- 	config = function()
-	-- 		require("numb").setup()
-	-- 	end,
-	-- })
-	-- use({
-	-- 	"lukas-reineke/indent-blankline.nvim",
-	-- 	config = require("config.indent-blankline").setup(),
-	-- })
+	--
+	use({
+		"is0n/fm-nvim",
+		-- config = "require('config.fm-nvim').setup()",
+	})
+	use({
+		"lewis6991/gitsigns.nvim",
+		-- config = function()
+		-- require("gitsigns").setup()
+		-- end,
+	})
+	use({
+		"norcalli/nvim-colorizer.lua",
+		-- config = function()
+		-- require("colorizer").setup()
+		-- end,
+	})
+	use({
+		"nacro90/numb.nvim",
+		-- config = function()
+		-- require("numb").setup()
+		-- end,
+	})
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		-- config = require("config.indent-blankline").setup(),
+	})
+	--
 	use("nvim-treesitter/nvim-treesitter-textobjects")
 	use("nvim-treesitter/playground")
 	-- use("windwp/nvim-ts-autotag")
