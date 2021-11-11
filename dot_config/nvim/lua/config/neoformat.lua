b479b4280d1443a3fe54ff0c55ed54e7991bc299
fuel <cha.cascio@gmail.com>
2021-11-11 08:38:57 +0100

    Update dot_config/nvim/lua/config/neoformat.lua

--- dot_config/nvim/lua/config/neoformat.lua
+++ dot_config/nvim/lua/config/neoformat.lua
@@ -4,12 +4,7 @@ vim.cmd("let g:neoformat_enabled_lua = ['stylua']")
 
 -- Format on write
 vim.cmd([[
-augroup fmt
-autocmd! au BufWritePre *.lua
-try | undojoin | Neoformat |
-catch /^Vim\%((\a\+)\)\=:E790/ |
-finally | silent Neoformat |
-endtry augroup END
+augroup fmt autocmd! au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry augroup END
 ]])
 
 -- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
