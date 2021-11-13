2594df0fe97794c1ff9610bff0e64bf6139655db
fuel <cha.cascio@gmail.com>
2021-11-13 19:22:43 +0100

    Add dot_config/nvim/lua/setups.lua

---
+++ dot_config/nvim/lua/setups.lua
@@ -0,0 +1,7 @@
+-------------------- inside/vim-search-pulse
+local function searchPulse()
+	g.vim_search_pulse_mode = "pattern"
+	g.vim_search_pulse_duration = 200
+end
+
+return { searchPulse = searchPulse }
