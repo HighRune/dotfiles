30c3f805474291291611b503b7dc3e8ebb693f2e
fuel <cha.cascio@gmail.com>
2021-11-13 21:26:14 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -38,6 +38,21 @@ local function tokyonight()
 	g.tokyonight_transparent_sidebar = true
 	cmd("colorscheme tokyonight")
 end
+-------------------- sbdchd/neoformat
+local function neoformat()
+	cmd("let g:neoformat_enabled_lua = ['stylua']") -- Enable lua formater
+	-- Format on write
+	cmd([[
+augroup fmt
+  autocmd!
+  au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+augroup END
+]])
+	-- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
+	-- vim.cmd("let g:neoformat_enabled_typescript = ['eslint_d']")
+	-- vim.api.nvim_command([[autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()]])
+	-- vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
+end
 
 return {
 	packer = packer,
@@ -46,4 +61,5 @@ return {
 	surround = surround,
 	wordmotion = wordmotion,
 	tokyonight = tokyonight,
+	neoformat = neoformat,
 }
