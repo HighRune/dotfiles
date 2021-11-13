e7ae91892f5b904ad428374314373f5139f14554
fuel <cha.cascio@gmail.com>
2021-11-13 17:16:18 +0100

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -20,7 +20,7 @@ return require("packer").startup(function(use)
 	})
 	use({
 		"nvim-telescope/telescope.nvim",
-		-- config = require("config.telescope").setup(),
+		config = require("config.telescope").setup(),
 	})
 	-- use({
 	-- 	"is0n/fm-nvim",
