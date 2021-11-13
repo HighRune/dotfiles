de607f03979fab0e0a20e8c8815b676a363718b0
fuel <cha.cascio@gmail.com>
2021-11-13 21:19:33 +0100

    Remove dot_config/nvim/lua/config/tokyonight.lua

--- dot_config/nvim/lua/config/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-local function setup()
-	vim.g.tokyonight_style = "night"
-	vim.g.tokyonight_transparent = true
-	vim.g.tokyonight_transparent_sidebar = true
-	vim.cmd("colorscheme tokyonight")
-end
-
-return { setup = setup }
