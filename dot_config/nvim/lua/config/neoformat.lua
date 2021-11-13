6825946b879e3894998e1b23eb167613d4657cff
fuel <cha.cascio@gmail.com>
2021-11-13 21:27:30 +0100

    Update dot_config/nvim/lua/config/neoformat.lua

--- dot_config/nvim/lua/config/neoformat.lua
+++ dot_config/nvim/lua/config/neoformat.lua
@@ -1,19 +1,19 @@
 local function setup()
--- Enable lua formater
-vim.cmd("let g:neoformat_enabled_lua = ['stylua']")
+	-- Enable lua formater
+	vim.cmd("let g:neoformat_enabled_lua = ['stylua']")
 
--- Format on write
-vim.cmd([[
+	-- Format on write
+	vim.cmd([[
 augroup fmt
   autocmd!
   au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
 augroup END
 ]])
 
--- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
--- vim.cmd("let g:neoformat_enabled_typescript = ['eslint_d']")
--- vim.api.nvim_command([[autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()]])
--- vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
+	-- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
+	-- vim.cmd("let g:neoformat_enabled_typescript = ['eslint_d']")
+	-- vim.api.nvim_command([[autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()]])
+	-- vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
 end
 
 return { setup = setup }
