748dee65e74b15d53f7f12a5f7f631798cc35b80
fuel <cha.cascio@gmail.com>
2021-11-13 20:33:21 +0100

    Update dot_config/nvim/lua/config/nvim-miniyank.lua

--- dot_config/nvim/lua/config/nvim-miniyank.lua
+++ dot_config/nvim/lua/config/nvim-miniyank.lua
@@ -1,10 +1,10 @@
 function setup()
-	-- local map = vim.api.nvim_set_keymap
 	-- require("mappings").miniyank()
-	-- map("", "p", "<Plug>(miniyank-autoput)", { silent = true })
-	-- map("", "P", "<Plug>(miniyank-autoPut)", { silent = true })
-	-- map("", "<c-n>", "<Plug>(miniyank-cycle)", { silent = true })
-	-- map("", "<c-p>", "<Plug>(miniyank-cycleback)", { silent = true })
+	local map = vim.api.nvim_set_keymap
+	map("", "p", "<Plug>(miniyank-autoput)", { silent = true })
+	map("", "P", "<Plug>(miniyank-autoPut)", { silent = true })
+	map("", "<c-n>", "<Plug>(miniyank-cycle)", { silent = true })
+	map("", "<c-p>", "<Plug>(miniyank-cycleback)", { silent = true })
 	vim.g.miniyank_maxitems = 5
 end
 
