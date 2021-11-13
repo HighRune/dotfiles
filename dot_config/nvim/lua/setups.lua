2be83679cfede470dc96295f154a7c7ff23ab532
fuel <cha.cascio@gmail.com>
2021-11-13 19:31:00 +0100

    Update dot_config/nvim/lua/setups.lua

--- dot_config/nvim/lua/setups.lua
+++ dot_config/nvim/lua/setups.lua
@@ -1,9 +1,22 @@
 local g = vim.g
+local cmd = vim.cmd
 
+-------------------- wbthomason/packer.nvim
+local function packer()
+	cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+end
 -------------------- inside/vim-search-pulse
 local function searchPulse()
 	g.vim_search_pulse_mode = "pattern"
 	g.vim_search_pulse_duration = 200
 end
 
-return { searchPulse = searchPulse }
+return {
+	packer = packer,
+	searchPulse = searchPulse,
+}
