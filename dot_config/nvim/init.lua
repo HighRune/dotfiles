933e34608a759c91b5b32c078018acb8607a9a66
fuel <cha.cascio@gmail.com>
2021-11-13 20:28:50 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -74,6 +74,13 @@ require("mappings").vanilla()
 
 require("plugins")
 
+local map = vim.api.nvim_set_keymap
+-- require("mappings").miniyank()
+map("", "p", "<Plug>(miniyank-autoput)", { silent = true })
+map("", "P", "<Plug>(miniyank-autoPut)", { silent = true })
+map("", "<c-n>", "<Plug>(miniyank-cycle)", { silent = true })
+map("", "<c-p>", "<Plug>(miniyank-cycleback)", { silent = true })
+
 -------------------- terryma/vim-expand-region
 -- cmd([[
 -- map <cr> <Plug>(expand_region_expand)
