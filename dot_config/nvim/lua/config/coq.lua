20b4c688a4e1ff752c1d403e9d1947b11c9006e7
fuel <cha.cascio@gmail.com>
2021-11-13 09:22:56 +0100

    Update dot_config/nvim/lua/config/coq.lua

--- dot_config/nvim/lua/config/coq.lua
+++ dot_config/nvim/lua/config/coq.lua
@@ -22,6 +22,7 @@ local function setup()
 			pum = {
 				kind_context = { " ", "" },
 				source_context = { "", "" },
+				fast_close = true,
 				-- x_truncate_len = 9999,
 			},
 			ghost_text = {
