a35fae8d0bcfa10b4c26abffd23e7ec805c5b67b
fuel <cha.cascio@gmail.com>
2021-11-13 18:48:53 +0100

    Update dot_config/nvim/lua/config/fm-nvim.lua

--- dot_config/nvim/lua/config/fm-nvim.lua
+++ dot_config/nvim/lua/config/fm-nvim.lua
@@ -1,18 +1,22 @@
-require("fm-nvim").setup({
-	border = "none",
-	height = 1,
-	width = 1,
-	-- Command used to open files: 'tabedit'; 'split'; 'pedit'; ...
-	edit_cmd = "edit",
-	-- Terminal commands used w/ file manager
-	cmds = {
-		vifm_cmd = "vifm",
-	},
-	-- Mappings used inside the floating window
-	mappings = {
-		vert_split = "<C-v>",
-		-- horz_split = "<C-h>",
-		-- tabedit = "<C-h>",
-		edit = "<C-e>",
-	},
-})
+local function setup()
+	require("fm-nvim").setup({
+		border = "none",
+		height = 1,
+		width = 1,
+		-- Command used to open files: 'tabedit'; 'split'; 'pedit'; ...
+		edit_cmd = "edit",
+		-- Terminal commands used w/ file manager
+		cmds = {
+			vifm_cmd = "vifm",
+		},
+		-- Mappings used inside the floating window
+		mappings = {
+			vert_split = "<C-v>",
+			-- horz_split = "<C-h>",
+			-- tabedit = "<C-h>",
+			edit = "<C-e>",
+		},
+	})
+end
+
+return { setup = setup }
