620e95650daa0c2de6bdf3b05e7cb9316fee157c
fuel <cha.cascio@gmail.com>
2021-11-14 09:22:32 +0100

    Update dot_config/nvim/init.lua Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,7 +1,139 @@
 require("options")
 require("mappings").vanilla()
 
-require("plugins")
+local mappings = require("mappings")
+local setup = require("setup")
+
+return require("packer").startup(function(use)
+	use({
+		"wbthomason/packer.nvim",
+		config = setup.packer(),
+	})
+	use("nvim-lua/plenary.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use({
+		"folke/tokyonight.nvim",
+		config = setup.tokyonight(),
+	})
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		config = require("config.treesitter").setup(),
+		run = ":TSUpdate",
+	})
+	use("neovim/nvim-lspconfig")
+	use({
+		"williamboman/nvim-lsp-installer",
+		config = "require('config.lsp').setup()",
+	})
+	use({
+		"ms-jpq/coq_nvim",
+		branch = "coq",
+		config = "require('config.coq').setup()",
+	})
+	use({
+		"nvim-telescope/telescope.nvim",
+		config = require("config.telescope").setup(),
+	})
+	use({
+		"is0n/fm-nvim",
+		config = function()
+			require("setup").fm()
+			require("mappings").fm()
+		end,
+	})
+	-- use({
+	-- 	"lewis6991/gitsigns.nvim",
+	-- 	config = function()
+	-- 		require("gitsigns").setup()
+	-- 	end,
+	-- })
+	-- use({
+	-- 	"norcalli/nvim-colorizer.lua",
+	-- 	config = function()
+	-- 		require("colorizer").setup()
+	-- 	end,
+	-- })
+	-- use({
+	-- 	"nacro90/numb.nvim",
+	-- 	config = function()
+	-- 		require("numb").setup()
+	-- 	end,
+	-- })
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = require("setup").indentBlankline(),
+	})
+	-- use("nvim-treesitter/nvim-treesitter-textobjects")
+	-- use("nvim-treesitter/playground")
+	-- use("windwp/nvim-ts-autotag")
+	-- use("itchyny/vim-cursorword")
+	use({
+		"romgrk/barbar.nvim",
+		config = mappings.barbar(),
+	})
+	use({ "ms-jpq/coq.artifacts", branch = "artifacts" })
+	use({
+		"sbdchd/neoformat",
+		config = setup.neoformat(),
+	})
+	use("tpope/vim-commentary")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	-- use("tommcdo/vim-lion")
+	-- use("p00f/nvim-ts-rainbow")
+	-- use({ "glepnir/galaxyline.nvim", config = "require('config.galaxyline').setup()" })
+	use({
+		"AndrewRadev/splitjoin.vim",
+		config = mappings.splitjoin(),
+	})
+	use({
+		"inside/vim-search-pulse",
+		config = setup.searchPulse(),
+	})
+	-- use("kana/vim-textobj-user")
+	-- use("rhysd/vim-textobj-anyblock")
+	-- use("AndrewRadev/sideways.vim")
+	use({
+		"bfredl/nvim-miniyank",
+		config = function()
+			require("setup").miniyank()
+			require("mappings").miniyank()
+		end,
+	})
+	-- use({
+	-- 	"windwp/nvim-autopairs",
+	-- 	-- config = require("config.nvim-autopairs").setup(),
+	-- })
+	use({
+		"blackCauldron7/surround.nvim",
+		config = function()
+			require("setup").surround()
+			require("mappings").surround()
+		end,
+	})
+	use({
+		"chaoren/vim-wordmotion",
+		config = function()
+			require("setup").wordmotion()
+			require("mappings").wordmotion()
+		end,
+	})
+
+	-- use("svermeulen/vimpeccable")
+	-- use("svermeulen/vim-yoink")
+	-- use("kana/vim-arpeggio")
+	-- use("matze/vim-move")
+	-- use("svermeulen/vim-cutlass")
+	-- use("RRethy/nvim-treesitter-textsubjects")
+	-- use { 'ibhagwan/fzf-lua', requires = { 'vijaymarupudi/nvim-fzf' } }
+	-- use({ "rrethy/vim-hexokinase", run = "make hexokinase", cmd = "HexokinaseTurnOn" })
+	-- use("tpope/vim-abolish")
+	-- use("tpope/vim-surround")
+	-- use("terryma/vim-expand-region")
+	-- use({
+	-- "ray-x/lsp_signature.nvim",
+	-- config = require("config.lsp_signature").setup(),
+	-- })
+end)
 
 -------------------- terryma/vim-expand-region
 -- cmd([[
