3dc6076abb082a7dcf2aa5cac082b3a7c43f5e51
fuel <cha.cascio@gmail.com>
2021-11-14 09:11:24 +0100

    Update dot_config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -4,37 +4,6 @@ local function setup()
 	-- after the language server attaches to the current buffer
 	local function on_attach(client, buffer)
 		require("mappings").lspconfig(buffer)
-		-- local keymaps = {
-		-- 	{ "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>" },
-		-- 	{ "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>" },
-		-- 	{ "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>" },
-		-- 	{ "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>" },
-		-- 	{ "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>" },
-		-- 	{ "n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>" },
-		-- 	{ "n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>" },
-		-- 	{ "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>" },
-		-- 	{ "n", "<leader>wd", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>" },
-		-- 	{ "n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>" },
-		-- 	{
-		-- 		"n",
-		-- 		"<leader>f",
-		-- 		"<cmd>lua vim.lsp.buf.formatting()<CR>",
-		-- 		-- "<cmd>lua vim.lsp.buf.formatting_seq_sync(nil, 6000, { 'tsserver', 'html', 'cssls', 'vuels', 'eslint' })<CR>",
-		-- 		-- "<cmd>lua vim.lsp.buf.formatting_seq_sync()<CR>",
-		-- 	},
-		-- 	{ "n", "<C-up>", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>" },
-		-- 	{ "n", "<C-down>", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>" },
-		-- 	{ "n", "<s-k>", "<cmd>lua vim.lsp.buf.hover()<CR>" },
-		-- 	{ "n", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>" },
-		-- 	-- { "n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>" },
-		-- 	-- { "n", "<leader>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>" },
-		-- }
-
-		-- local opts = { noremap = true, silent = true }
-
-		-- for _, keymap in ipairs(keymaps) do
-		-- 	vim.api.nvim_buf_set_keymap(buffer, keymap[1], keymap[2], keymap[3], opts)
-		-- end
 
 		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 			update_in_insert = true,
