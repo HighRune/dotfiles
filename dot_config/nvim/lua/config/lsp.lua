95a21893b04b2811f73ba48d1cd6a8ca9178445b
fuel <cha.cascio@gmail.com>
2021-11-15 21:39:15 +0100

    Update dot_config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -21,16 +21,18 @@ local function setup()
 		end
 
 		local border = {
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
-			{ "*", "FloatBorder" },
+			{ "┌", "FloatBorder" },
+			{ "─", "FloatBorder" },
+			{ "┐", "FloatBorder" },
+			{ "│", "FloatBorder" },
+			{ "┘", "FloatBorder" },
+			{ "─", "FloatBorder" },
+			{ "└", "FloatBorder" },
+			{ "│", "FloatBorder" },
 		}
+
 		local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
 		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 			opts = opts or {}
 			opts.border = opts.border or border
