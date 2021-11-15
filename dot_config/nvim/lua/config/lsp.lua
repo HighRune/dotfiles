d838f6dbe46fecea2f7f2d1de613cbdb60681e79
fuel <cha.cascio@gmail.com>
2021-11-15 21:27:25 +0100

    Update dot_config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -19,6 +19,23 @@ local function setup()
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 		end
+
+		local border = {
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+			{ "*", "FloatBorder" },
+		}
+		local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+			opts = opts or {}
+			opts.border = opts.border or border
+			return orig_util_open_floating_preview(contents, syntax, opts, ...)
+		end
 	end
 
 	local function on_attach_eslint(client, buffer)
