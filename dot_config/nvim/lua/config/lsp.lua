8f105cf407aa13460bd5570b62a58fb597a4be39
fuel <cha.cascio@gmail.com>
2021-11-15 18:32:47 +0100

    Update dot_config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -13,11 +13,11 @@ local function setup()
 		-- 	},
 		-- })
 
-		local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 		for type, icon in pairs(signs) do
-			local hl = "LspDiagnosticsSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl })
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 		end
 	end
 
