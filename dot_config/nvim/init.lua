b8c3eb84e69cfc4ba1ebc528100baf5bc1ba555e
fuel <cha.cascio@gmail.com>
2021-11-30 22:54:36 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,10 +12,11 @@ return require("packer").startup({
 		})
 		use("nvim-lua/plenary.nvim")
 		use("kyazdani42/nvim-web-devicons")
-		use({
-			"folke/tokyonight.nvim",
-			config = require("setup").tokyonight(),
-		})
+		-- use({
+		-- 	"folke/tokyonight.nvim",
+		-- 	-- config = require("setup").tokyonight(),
+		-- })
+		use({ "ishan9299/nvim-solarized-lua" })
 		use({
 			"nvim-treesitter/nvim-treesitter",
 			config = require("config.treesitter").setup(),
