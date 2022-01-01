bee01c3c34aec4722f1ca8145c500adb9463b287
fuel <cha.cascio@gmail.com>
2022-01-01 23:02:26 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -81,16 +81,49 @@ end
 -------------------- is0n/fm-nvim
 local function fm()
 	require("fm-nvim").setup({
-		border = "none",
-		height = 999,
-		width = 999,
-		-- Command used to open files: 'tabedit'; 'split'; 'pedit'; ...
+		-- (Vim) Command used to open files
 		edit_cmd = "edit",
-		-- Terminal commands used w/ file manager
+
+		-- UI Options
+		ui = {
+			-- Default UI (can be "split" or "float")
+			default = "float",
+
+			float = {
+				-- Floating window border (see ':h nvim_open_win')
+				border = "none",
+
+				-- Highlight group for floating window/border (see ':h winhl')
+				float_hl = "Normal",
+				border_hl = "FloatBorder",
+
+				-- Floating Window Transparency (see ':h winblend')
+				blend = 0,
+
+				-- Num from 0 - 1 for measurements
+				height = 999,
+				width = 999,
+
+				-- X and Y Axis of Window
+				-- x = 0.5,
+				-- y = 0.5,
+			},
+
+			split = {
+				-- Direction of split
+				direction = "topleft",
+
+				-- Size of split
+				size = 24,
+			},
+		},
+
+		-- Terminal commands used w/ file manager (have to be in your $PATH)
 		cmds = {
 			vifm_cmd = "vifm",
 		},
-		-- Mappings used inside the floating window
+
+		-- Mappings used with the plugin
 		mappings = {
 			-- vert_split = "<C-v>",
 			-- horz_split = "<C-h>",
