d851d19cee92489b06ce3971898d9e0719fea06c
fuel <cha.cascio@gmail.com>
2022-01-02 18:08:03 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -14,7 +14,7 @@ end
 local function bufferline()
 	require("bufferline").setup({
 		options = {
-			indicator_icon = " ",
+			indicator_icon = "",
 			buffer_close_icon = "",
 			modified_icon = "●",
 			close_icon = "",
