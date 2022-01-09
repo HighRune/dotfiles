689e748515f81b96f624b35771b9b35ec0925fe0
fuel <cha.cascio@gmail.com>
2022-01-09 10:30:59 +0100

    Update dot_config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -61,6 +61,7 @@ local function vanilla()
 	map("o", "W", "iW", {})
 	map("o", "b", "ib", {})
 	map("o", "l", "al", {})
+	-- inoremap <Esc> <Esc>`^
 	-- Cut
 	map("x", "d", "ygvd", opts)
 	map("x", "c", "ygvc", opts)
