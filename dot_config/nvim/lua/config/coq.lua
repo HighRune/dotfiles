d4aea1cbaf1dd6b01fcc83f326b8feb62a40af96
fuel <cha.cascio@gmail.com>
2022-01-09 21:46:27 +0100

    Update dot_config/nvim/lua/config/coq.lua

--- dot_config/nvim/lua/config/coq.lua
+++ dot_config/nvim/lua/config/coq.lua
@@ -41,6 +41,16 @@ local function setup()
 			["repeat"] = "<leader>.",
 		},
 	}
+	vim.cmd([[
+let g:coq_settings = { "keymap.recommended": v:false }
+
+ino <silent><expr> <Esc>   pumvisible() ? "\<C-e><Esc>" : "\<Esc>`^"
+ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
+ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+ino <silent><expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
+  ]])
 end
 
 return { setup = setup }
