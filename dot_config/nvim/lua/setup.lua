0483bad3695553d22bfdc187e529a17f02c07a8d
fuel <cha.cascio@gmail.com>
2022-01-12 23:24:50 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -97,39 +97,16 @@ local function scrollbar()
 	local colors = require("tokyonight.colors").setup()
 
 	require("scrollbar").setup({
-		show = true,
 		handle = {
-			text = "|",
-			color = "none",
+			color = colors.bg_highlight,
 		},
 		marks = {
-			Search = { color = colors.orange },
-			Error = { color = colors.error },
-			Warn = { color = colors.warning },
-			Info = { color = colors.info },
-			Hint = { color = colors.hint },
-			Misc = { color = colors.purple },
-		},
-		excluded_filetypes = {
-			"",
-			"prompt",
-			"TelescopePrompt",
-		},
-		autocmd = {
-			render = {
-				"BufWinEnter",
-				"TabEnter",
-				"TermEnter",
-				"WinEnter",
-				"CmdwinLeave",
-				"TextChanged",
-				"VimResized",
-				"WinScrolled",
-			},
-		},
-		handlers = {
-			diagnostic = true,
-			search = false, -- Requires hlslens to be loaded
+			Search = { text = { ".", "." }, color = colors.orange },
+			Error = { text = { ".", "." }, color = colors.error },
+			Warn = { text = { ".", "." }, color = colors.warning },
+			Info = { text = { ".", "." }, color = colors.info },
+			Hint = { text = { ".", "." }, color = colors.hint },
+			Misc = { text = { ".", "." }, color = colors.purple },
 		},
 	})
 end
