3ef75d40d6e5786aab9925e6bb90c093f720df00
fuel <cha.cascio@gmail.com>
2022-01-12 22:18:31 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -154,6 +154,10 @@ return require("packer").startup({
 			requires = { "kyazdani42/nvim-web-devicons", opt = true },
 			config = "require('config.lualine').setup()",
 		})
+		use({
+			"petertriho/nvim-scrollbar",
+			-- config = require("scrollbar").setup(),
+		})
 		-- use({
 		-- 	"famiu/feline.nvim",
 		-- 	config = "require('feline').setup()",
