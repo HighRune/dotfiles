4f3f6ed4ca88745a71df81221513a446958cf164
fuel <cha.cascio@gmail.com>
2022-01-18 22:36:03 +0100

    Update dot_config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -3,11 +3,10 @@ local g = vim.g
 local opt = vim.opt
 local cmd = vim.cmd
 
-cmd([[autocmd ColorScheme * highlight NormalFloat guibg=none]])
-cmd([[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=none]])
+-- cmd([[autocmd ColorScheme * highlight NormalFloat guibg=none]])
+-- cmd([[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=none]])
 cmd([[autocmd ColorScheme * highlight TelescopeBorder guibg=none]])
 cmd([[autocmd ColorScheme * highlight TelescopeNormal guibg=none]])
--- cmd([[autocmd ColorScheme * highlight FloatBorder guifg=blue guibg=none]])
 
 -- cmd([[color haslo]])
 -- o.colorscheme darkspace
