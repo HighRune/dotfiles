094e48d797b4e98b0a4e3f635e2514025f9796a8
fuel <cha.cascio@gmail.com>
2022-01-21 21:48:40 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -145,7 +145,13 @@ return require("packer").startup({
 			requires = { "kyazdani42/nvim-web-devicons", opt = true },
 			config = "require('config.lualine').setup()",
 		})
-		use("rlane/pounce.nvim")
+		use({
+			"rlane/pounce.nvim",
+			config = function()
+				require("setup").pounce()
+				require("mappings").pounce()
+			end,
+		})
 		-- use({
 		-- 	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 		-- 	config = function()
