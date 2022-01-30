a58f3e3dd3c918a0e364232090d8f3ee54aa1363
fuel <cha.cascio@gmail.com>
2022-01-30 21:57:57 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -244,10 +244,10 @@ local function fzf()
 				["<C-e>"] = "preview-page-down",
 				["<C-u>"] = "preview-page-up",
 			},
-			fzf = {
-				["ctrl-e"] = "preview-page-down",
-				["ctrl-u"] = "preview-page-up",
-			},
+			-- fzf = {
+			-- 	["ctrl-e"] = "preview-page-down",
+			-- 	["ctrl-u"] = "preview-page-up",
+			-- },
 		},
 	})
 end
