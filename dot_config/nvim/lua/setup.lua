523599a468aeeb00f5fc392789d036f2cf270654
fuel <cha.cascio@gmail.com>
2022-02-10 23:35:37 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -83,20 +83,20 @@ local function solarized()
 end
 
 -------------------- sbdchd/neoformat
--- local function neoformat()
--- 	cmd("let g:neoformat_enabled_lua = ['stylua']") -- Enable lua formater
--- 	-- Format on write
--- 	cmd([[
--- augroup fmt
---   autocmd!
---   au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
--- augroup END
--- ]])
--- 	-- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
--- 	-- vim.cmd("let g:neoformat_enabled_typescript = ['eslint_d']")
--- 	-- vim.api.nvim_command([[autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()]])
--- 	-- vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
--- end
+local function neoformat()
+	cmd("let g:neoformat_enabled_lua = ['stylua']") -- Enable lua formater
+	-- Format on write
+	cmd([[
+augroup fmt
+  autocmd!
+  au BufWritePre *.lua try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
+augroup END
+]])
+	-- vim.cmd("let g:neoformat_enabled_javascript = ['eslint_d']")
+	-- vim.cmd("let g:neoformat_enabled_typescript = ['eslint_d']")
+	-- vim.api.nvim_command([[autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()]])
+	-- vim.api.nvim_set_keymap("n", "<leader>f", ":Neoformat<CR>", { noremap = true })
+end
 
 -------------------- lewis6991/gitsigns.nvim
 local function gitsigns()
@@ -291,7 +291,7 @@ return {
 	wordmotion = wordmotion,
 	tokyonight = tokyonight,
 	solarized = solarized,
-	-- neoformat = neoformat,
+	neoformat = neoformat,
 	fm = fm,
 	indentBlankline = indentBlankline,
 	hop = hop,
