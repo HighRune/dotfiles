68d3886c8400b72208b356b0baf501cb6e1400a5
fuel <cha.cascio@gmail.com>
2022-02-15 19:25:46 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -272,6 +272,14 @@ end
 
 -------------------- wellle/targets.vim
 local function targets()
+	cmd([[
+	autocmd User targets#mappings#user call targets#mappings#extend({
+	\ 'a': {'argument': [{'o': '(', 'c': ')', 's': ','}]},
+	\ 'A': {'argument': [{'o': '{', 'c': '}', 's': ','}]},
+	\ 'b': {'pair': [{'o':'(', 'c':')'}]},
+	\ 'B': {'pair': [{'o':'{', 'c': '}'}]},
+	\ })
+	]])
 	-- cmd([[
 	-- autocmd User targets#mappings#user call targets#mappings#extend({
 	-- \ 'a': {'argument': [{'o': '[[(]', 'c': '[])]', 's': ','}]},
