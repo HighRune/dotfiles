1110beee8b743b504020a55d69db7f9b0a686db5
fuel <cha.cascio@gmail.com>
2022-02-25 22:42:48 +0100

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -58,9 +58,9 @@ end
 
 -------------------- chaoren/vim-wordmotion
 local function wordmotion()
-	-- let g:wordmotion_nomap = 1
 	cmd([[
-	let g:wordmotion_spaces = '_-.:'
+	let g:wordmotion_nomap = 1
+	let g:wordmotion_uppercase_spaces = ',/?_-.:<>()\[\]{}&*=!+"'''
 	]])
 end
 
