1cdb454250bcaeaf0d261cc4c6d393740a0151ae
fuel <cha.cascio@gmail.com>
2022-03-08 23:12:28 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -77,21 +77,21 @@ return require("packer").startup({
 		use({
 			"akinsho/bufferline.nvim",
 			config = function()
-				require("setup").bufferline()
-				require("mappings").bufferline()
+				-- require("setup").bufferline()
+				-- require("mappings").bufferline()
 			end,
 		})
 		use({ "ms-jpq/coq.artifacts", branch = "artifacts" })
 		use({
 			"sbdchd/neoformat",
-			config = require("setup").neoformat(),
+			-- config = require("setup").neoformat(),
 		})
 		-- use("tpope/vim-commentary")
 		-- use("JoosepAlviste/nvim-ts-context-commentstring")
 		use({
 			"numToStr/Comment.nvim",
 			config = function()
-				require("Comment").setup()
+				-- require("Comment").setup()
 			end,
 		})
 		use("tommcdo/vim-lion")
@@ -100,8 +100,8 @@ return require("packer").startup({
 		use({
 			"AndrewRadev/splitjoin.vim",
 			config = function()
-				require("mappings").splitjoin()
-				require("setup").splitjoin()
+				-- require("mappings").splitjoin()
+				-- require("setup").splitjoin()
 			end,
 		})
 		-- use({
@@ -115,49 +115,49 @@ return require("packer").startup({
 		use({
 			"wellle/targets.vim",
 			config = function()
-				require("setup").targets()
-				require("mappings").targets()
+				-- require("setup").targets()
+				-- require("mappings").targets()
 			end,
 		})
 		-- use("kana/vim-textobj-user")
 		-- use("PeterRincker/vim-argumentative")
 		use({
 			"windwp/nvim-autopairs",
-			config = require("config.nvim-autopairs").setup(),
+			-- config = require("config.nvim-autopairs").setup(),
 		})
 		use({
 			"chaoren/vim-wordmotion",
 			config = function()
-				require("setup").wordmotion()
-				require("mappings").wordmotion()
+				-- require("setup").wordmotion()
+				-- require("mappings").wordmotion()
 			end,
 		})
 		use({
 			"kana/vim-arpeggio",
-			require("mappings").arpeggio(),
+			-- require("mappings").arpeggio(),
 		})
 		use({
 			"ahmedkhalf/project.nvim",
-			config = function()
-				require("project_nvim").setup()
-			end,
+			-- config = function()
+			-- require("project_nvim").setup()
+			-- end,
 		})
 		use({
 			"nvim-lualine/lualine.nvim",
-			requires = { "kyazdani42/nvim-web-devicons", opt = true },
-			config = "require('config.lualine').setup()",
+			-- requires = { "kyazdani42/nvim-web-devicons", opt = true },
+			-- config = "require('config.lualine').setup()",
 		})
 		use({
 			"gbprod/cutlass.nvim",
 			config = function()
-				require("setup").cutlass()
+				-- require("setup").cutlass()
 			end,
 		})
 		use({
 			"justinmk/vim-sneak",
 			config = function()
-				require("setup").sneak()
-				require("mappings").sneak()
+				-- require("setup").sneak()
+				-- require("mappings").sneak()
 			end,
 		})
 		-- use({
@@ -169,22 +169,22 @@ return require("packer").startup({
 		use({
 			"ibhagwan/fzf-lua",
 			config = function()
-				require("setup").fzf()
-				require("mappings").fzf()
+				-- require("setup").fzf()
+				-- require("mappings").fzf()
 			end,
 		})
 		use({
 			"monaqa/dial.nvim",
 			config = function()
-				require("setup").dial()
-				require("mappings").dial()
+				-- require("setup").dial()
+				-- require("mappings").dial()
 			end,
 		})
 		use("tpope/vim-repeat")
 		use({
 			"machakann/vim-highlightedyank",
 			config = function()
-				require("setup").highlightedyank()
+				-- require("setup").highlightedyank()
 			end,
 		})
 		use("svban/YankAssassin.vim")
