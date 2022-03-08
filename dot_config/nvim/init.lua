a0c90fc9cd8a33cdfb055ea4aa27d116c454d184
fuel <cha.cascio@gmail.com>
2022-03-08 22:57:57 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -169,8 +169,8 @@ return require("packer").startup({
 		use({
 			"ibhagwan/fzf-lua",
 			config = function()
-				require("setup").fzf()
-				require("mappings").fzf()
+				-- require("setup").fzf()
+				-- require("mappings").fzf()
 			end,
 		})
 		use({
