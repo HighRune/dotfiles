173841a458aa8a762f82ec1c2fb2d925ad12bd02
fuel <cha.cascio@gmail.com>
2022-03-12 00:11:32 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -40,7 +40,7 @@ return require("packer").startup({
 		use({
 			"williamboman/nvim-lsp-installer",
 			-- config = "require('config.lsp').setup()",
-			disable = true,
+			cond = true,
 		})
 		-- use({
 		-- 	"ms-jpq/coq_nvim",
