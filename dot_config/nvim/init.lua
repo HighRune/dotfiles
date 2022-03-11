18a3224d4a09eb389593217280c7bf740cdb5d64
fuel <cha.cascio@gmail.com>
2022-03-12 00:08:59 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -27,12 +27,12 @@ return require("packer").startup({
 			-- config = require("setup").tokyonight(),
 			cond = true,
 		})
-		-- use({
-		-- 	"nvim-treesitter/nvim-treesitter",
-		-- 	config = require("config.treesitter").setup(),
-		-- 	run = ":TSUpdate",
-		-- 	disable = true,
-		-- })
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			-- config = require("config.treesitter").setup(),
+			run = ":TSUpdate",
+			cond = true,
+		})
 		use({
 			"neovim/nvim-lspconfig",
 			cond = false,
