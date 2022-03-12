96294f1f355fd9f4593c9849e75e78b2bd6f1aa9
fuel <cha.cascio@gmail.com>
2022-03-12 19:55:22 +0100

    Update dot_config/nvim/lua/config.lua

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -1,1 +1,9 @@
---
+-------------------- wbthomason/packer.nvim
+local function packer()
+	cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+end
