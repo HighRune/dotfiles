fdfc4337d98d521be8f87f723fef932ff1ffa05f
fuel <cha.cascio@gmail.com>
2022-03-13 17:00:07 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -106,7 +106,7 @@ return require("packer").startup({
 		use({
 			"nvim-lualine/lualine.nvim",
 			requires = { "kyazdani42/nvim-web-devicons", opt = true },
-			config = "require('config.lualine').setup()",
+			config = "require('config.lualine')()",
 		})
 		use({
 			"gbprod/cutlass.nvim",
