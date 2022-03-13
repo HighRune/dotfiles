c1a64e2441db6e64fe95cb686b6fd4e7fc3b8bf6
fuel <cha.cascio@gmail.com>
2022-03-13 16:07:43 +0100

    Update dot_config/nvim/lua/config.lua

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -19,7 +19,40 @@ local function tokyonight()
 	cmd("colorscheme tokyonight")
 end
 
+-------------------- is0n/fm-nvim
+local function fm()
+	require("fm-nvim").setup({
+		edit_cmd = "edit",
+		ui = {
+			default = "float",
+			float = {
+				border = "none",
+				float_hl = "Normal",
+				border_hl = "FloatBorder",
+				blend = 0,
+				height = 999,
+				width = 999,
+			},
+			split = {
+				direction = "topleft",
+				size = 24,
+			},
+		},
+		cmds = {
+			vifm_cmd = "vifm",
+		},
+		mappings = {
+			-- vert_split = "<C-v>",
+			-- horz_split = "<C-h>",
+			-- tabedit = "<C-t>",
+			-- edit = "<C-e>",
+			-- ESC = "<ESC>",
+		},
+	})
+end
+
 return {
 	packer = packer,
 	tokyonight = tokyonight,
+	fm = fm,
 }
