bc77c6d5bd65bb2b195f2c0bd12cc3a6f0570752
fuel <cha.cascio@gmail.com>
2022-03-13 10:31:00 +0100

    Update dot_config/nvim/lua/config/lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -5,103 +5,112 @@ local diagnostic = vim.diagnostic
 
 -------------------- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
 
-diagnostic.config({
-	-- virtual_text = false,
-	-- virtual_lines = { prefix = "" },
-	virtual_text = {
-		prefix = "",
-		spacing = 2,
-	},
-	signs = false,
-	float = {
-		header = "",
-		prefix = "",
-		format = function(diagnostic)
-			return string.format(" %s %s\n%s", diagnostic.source, diagnostic.user_data.lsp.code, diagnostic.message)
-			-- return dump(diagnostic)
-		end,
-	},
-})
+local function setup()
+	diagnostic.config({
+		-- virtual_text = false,
+		-- virtual_lines = { prefix = "" },
+		virtual_text = {
+			prefix = "",
+			spacing = 2,
+		},
+		signs = false,
+		float = {
+			header = "",
+			prefix = "",
+			format = function(diagnostic)
+				return string.format(
+					" %s %s\n%s",
+					diagnostic.source,
+					diagnostic.user_data.lsp.code,
+					diagnostic.message
+				)
+				-- return dump(diagnostic)
+			end,
+		},
+	})
 
--- function dump(o)
--- 	if type(o) == "table" then
--- 		local s = "{ "
--- 		for k, v in pairs(o) do
--- 			if type(k) ~= "number" then
--- 				k = '"' .. k .. '"'
--- 			end
--- 			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
--- 		end
--- 		return s .. "} "
--- 	else
--- 		return tostring(o)
--- 	end
--- end
-
--- local signs = { Error = "e", Warn = "w", Hint = "h", Info = "i" }
--- for type, sign in pairs(signs) do
--- 	local hl = "DiagnosticSign" .. type
--- 	fn.sign_define(hl, { text = sign, texthl = hl })
--- end
-
--------------------- neovim/nvim-lspconfig
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local function on_attach(client, buffer)
-	require("mappings").lspconfig(buffer)
-end
-
-local function on_attach_eslint(client, buffer)
-	client.resolved_capabilities.document_formatting = true
-	on_attach(client, buffer)
-end
-
-local function on_attach_tsserver(client, buffer)
-	client.resolved_capabilities.document_formatting = false
-	on_attach(client, buffer)
-end
-
-local function on_attach_volar(client, buffer)
-	client.resolved_capabilities.document_formatting = false
-	on_attach(client, buffer)
-end
-
-local function on_attach_sumneko_lua(client, buffer)
-	client.resolved_capabilities.document_formatting = true
-	on_attach(client, buffer)
-end
--------------------- williamboman/nvim-lsp-installer
-require("nvim-lsp-installer").on_server_ready(function(server)
-	local opts = {}
-	opts.on_attach = on_attach
-	opts.flags = { debounce_text_changes = 0 }
-
-	-- (optional) Customize the options passed to the server
-	-- if server.name == "tsserver" then
-	--     opts.root_dir = function() ... end
+	-- function dump(o)
+	-- 	if type(o) == "table" then
+	-- 		local s = "{ "
+	-- 		for k, v in pairs(o) do
+	-- 			if type(k) ~= "number" then
+	-- 				k = '"' .. k .. '"'
+	-- 			end
+	-- 			s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+	-- 		end
+	-- 		return s .. "} "
+	-- 	else
+	-- 		return tostring(o)
+	-- 	end
 	-- end
 
-	if server.name == "eslint" then
-		opts.on_attach = on_attach_eslint
-		opts.settings = { format = { enable = true } }
+	-- local signs = { Error = "e", Warn = "w", Hint = "h", Info = "i" }
+	-- for type, sign in pairs(signs) do
+	-- 	local hl = "DiagnosticSign" .. type
+	-- 	fn.sign_define(hl, { text = sign, texthl = hl })
+	-- end
+
+	-------------------- neovim/nvim-lspconfig
+	-- Use an on_attach function to only map the following keys
+	-- after the language server attaches to the current buffer
+	local function on_attach(client, buffer)
+		require("mappings").lspconfig(buffer)
 	end
 
-	if server.name == "tsserver" then
-		opts.on_attach = on_attach_tsserver
-		opts.settings = { format = { enable = false } }
+	local function on_attach_eslint(client, buffer)
+		client.resolved_capabilities.document_formatting = true
+		on_attach(client, buffer)
 	end
 
-	if server.name == "volar" then
-		opts.on_attach = on_attach_volar
-		opts.settings = { format = { enable = false } }
+	local function on_attach_tsserver(client, buffer)
+		client.resolved_capabilities.document_formatting = false
+		on_attach(client, buffer)
 	end
 
-	if server.name == "sumneko_lua" then
-		opts.on_attach = on_attach_sumneko_lua
-		opts.settings = { format = { enable = true } }
+	local function on_attach_volar(client, buffer)
+		client.resolved_capabilities.document_formatting = false
+		on_attach(client, buffer)
 	end
 
-	-- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
-	server:setup(require("coq").lsp_ensure_capabilities(opts))
-	cmd([[ do User LspAttachBuffers ]])
-end)
+	local function on_attach_sumneko_lua(client, buffer)
+		client.resolved_capabilities.document_formatting = true
+		on_attach(client, buffer)
+	end
+	-------------------- williamboman/nvim-lsp-installer
+	require("nvim-lsp-installer").on_server_ready(function(server)
+		local opts = {}
+		opts.on_attach = on_attach
+		opts.flags = { debounce_text_changes = 0 }
+
+		-- (optional) Customize the options passed to the server
+		-- if server.name == "tsserver" then
+		--     opts.root_dir = function() ... end
+		-- end
+
+		if server.name == "eslint" then
+			opts.on_attach = on_attach_eslint
+			opts.settings = { format = { enable = true } }
+		end
+
+		if server.name == "tsserver" then
+			opts.on_attach = on_attach_tsserver
+			opts.settings = { format = { enable = false } }
+		end
+
+		if server.name == "volar" then
+			opts.on_attach = on_attach_volar
+			opts.settings = { format = { enable = false } }
+		end
+
+		if server.name == "sumneko_lua" then
+			opts.on_attach = on_attach_sumneko_lua
+			opts.settings = { format = { enable = true } }
+		end
+
+		-- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+		server:setup(require("coq").lsp_ensure_capabilities(opts))
+		cmd([[ do User LspAttachBuffers ]])
+	end)
+end
+
+return { setup = setup }
