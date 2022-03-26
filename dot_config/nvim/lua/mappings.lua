3d7eeab551ec3ace17657cea60c7a539ab8e3762
HighRune <cha.cascio@gmail.com>
2022-03-26 20:01:46 +0100

    Update dot_config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -35,10 +35,8 @@ local function core()
 	map("n", "$", "g$:set ve= ve=all<cr>")
 	map("n", "&", "g^")
 	cmd([[
-nnoremap <silent><C-j> m`:silent +g/\m^\s*$/d<CR>``:noh<CR>
-nnoremap <silent><C-k> m`:silent -g/\m^\s*$/d<CR>``:noh<CR>
-nnoremap <silent><A-j> :set paste<CR>m`o<Esc>``:set nopaste<CR>
-nnoremap <silent><A-k> :set paste<CR>m`O<Esc>``:set nopaste<CR>
+nnoremap <silent><C-k> m`:silent +g/\m^\s*$/d<CR>``:noh<CR>
+nnoremap <silent><C-j> :set paste<CR>m`o<Esc>``:set nopaste<CR>
 	]])
 	map("x", "p", '"_dP')
 	-- map("n", "gm", "(virtcol('$') / 2) . '<Bar>'", { expr = true })
