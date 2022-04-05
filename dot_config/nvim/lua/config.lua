995d131e19bc9ab0a269a5b556aa2b4577c5c440
HighRune <cha.cascio@gmail.com>
2022-04-05 20:08:51 +0200

    Update dot_config/nvim/lua/config.lua

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -236,6 +236,8 @@ end
 -------------------- chaoren/vim-wordmotion
 local function wordmotion()
 	local spaces = {
+		"_",
+		"-",
 		",",
 		"/",
 		".",
@@ -262,7 +264,7 @@ local function wordmotion()
 	g.wordmotion_nomap = 1
 	-- Move to the character following those characters
 	g.wordmotion_uppercase_spaces = spaces
-	g.wordmotion_spaces = spaces
+	-- g.wordmotion_spaces = spaces
 end
 
 -------------------- kana/vim-textobj-user
