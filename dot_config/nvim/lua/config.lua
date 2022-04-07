384431eae3ed443e8c3a23308d818a181459ee77
HighRune <cha.cascio@gmail.com>
2022-04-07 19:07:00 +0200

    Update dot_config/nvim/lua/config.lua

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -344,10 +344,11 @@ end
 
 -------------------- David-Kunz/treesitter-unit
 local function unit()
-vim.api.nvim_set_keymap('x', 'iu', ':lua require"treesitter-unit".select()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('x', 'au', ':lua require"treesitter-unit".select(true)<CR>', {noremap=true})
-vim.api.nvim_set_keymap('o', 'iu', ':<c-u>lua require"treesitter-unit".select()<CR>', {noremap=true})
-vim.api.nvim_set_keymap('o', 'au', ':<c-u>lua require"treesitter-unit".select(true)<CR>', {noremap=true})
+	require("treesitter-unit").toggle_highlighting()
+	vim.api.nvim_set_keymap("x", "iu", ':lua require"treesitter-unit".select()<CR>', { noremap = true })
+	vim.api.nvim_set_keymap("x", "au", ':lua require"treesitter-unit".select(true)<CR>', { noremap = true })
+	vim.api.nvim_set_keymap("o", "iu", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
+	vim.api.nvim_set_keymap("o", "au", ':<c-u>lua require"treesitter-unit".select(true)<CR>', { noremap = true })
 end
 
 -------------------- kana/vim-submode
