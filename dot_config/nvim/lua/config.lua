28531bec43a0b8e99cac2f15382a8961f16bd7f4
HighRune <cha.cascio@gmail.com>
2022-04-07 19:02:37 +0200

    Update dot_config/nvim/lua/config.lua

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -342,6 +342,14 @@ local function highlightedyank()
 	cmd("let g:highlightedyank_highlight_duration = 100")
 end
 
+-------------------- David-Kunz/treesitter-unit
+local function unit()
+vim.api.nvim_set_keymap('x', 'iu', ':lua require"treesitter-unit".select()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('x', 'au', ':lua require"treesitter-unit".select(true)<CR>', {noremap=true})
+vim.api.nvim_set_keymap('o', 'iu', ':<c-u>lua require"treesitter-unit".select()<CR>', {noremap=true})
+vim.api.nvim_set_keymap('o', 'au', ':<c-u>lua require"treesitter-unit".select(true)<CR>', {noremap=true})
+end
+
 -------------------- kana/vim-submode
 local function submode()
 	cmd([[
@@ -381,4 +389,5 @@ return {
 	indentwise = indentwise,
 	textobjuser = textobjuser,
 	submode = submode,
+	unit = unit,
 }
