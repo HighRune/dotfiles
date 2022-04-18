146ad0a41e0d0e9471082bf0c8646cb5e0944005
HighRune <cha.cascio@gmail.com>
2022-04-18 19:21:02 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -132,7 +132,7 @@ return require("packer").startup({
 			config = config.submode(),
 		})
 		use({
-			"/home/charles/Documents/dev/plugins/putline",
+			"/home/charles/Documents/dev/plugins/putline.nvim",
 			config = require("putline").setup(),
 		})
 	end,
