e49a0cbd7e290527e93078f5b5d6268ee7c40edf
HighRune <cha.cascio@gmail.com>
2022-04-18 19:07:33 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -131,7 +131,7 @@ return require("packer").startup({
 			"kana/vim-submode",
 			config = config.submode(),
 		})
-		-- use("/home/charles/Documents/dev/plugins/putline.nvim")
+		use("/home/charles/Documents/dev/plugins/putline.nvim")
 	end,
 	config = {
 		display = {
