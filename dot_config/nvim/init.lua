250610392075e69dd631bfaf114ef163f9541a78
HighRune <cha.cascio@gmail.com>
2022-04-18 17:12:18 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -31,10 +31,10 @@ return require("packer").startup({
 			config = require("config.coq")(),
 			requires = { "ms-jpq/coq.artifacts", branch = "artifacts" },
 		})
-		use({
-			"is0n/fm-nvim",
-			config = config.fm(),
-		})
+		-- use({
+		-- 	"is0n/fm-nvim",
+		-- 	config = config.fm(),
+		-- })
 		use({
 			"lewis6991/gitsigns.nvim",
 			config = config.gitsigns(),
