0fd4c246e9d5a42d4627b1305efc132528146ff6
HighRune <cha.cascio@gmail.com>
2022-04-28 20:40:53 +0200

    Update dot_config/nvim/lua/config/init.lua

--- dot_config/nvim/lua/config/init.lua
+++ dot_config/nvim/lua/config/init.lua
@@ -269,10 +269,35 @@ local function wordmotion()
 		"'",
 		'"',
 	}
+	local upspaces = {
+		-- "_",
+		"-",
+		",",
+		"/",
+		".",
+		":",
+		"<",
+		">",
+		"(",
+		")",
+		"[",
+		"]",
+		"{",
+		"}",
+		"&",
+		"@",
+		"*",
+		"=",
+		"!",
+		"+",
+		"?",
+		"'",
+		'"',
+	}
 	require("mappings").wordmotion()
 	g.wordmotion_nomap = 1
 	-- Move to the character following those characters
-	g.wordmotion_uppercase_spaces = spaces
+	g.wordmotion_uppercase_spaces = upspaces
 	g.wordmotion_spaces = spaces
 end
 
