5239d95a788dfcd6c0695edb09e42d343fdf6db1
HighRune <cha.cascio@gmail.com>
2022-04-28 21:32:34 +0200

    Update dot_config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -1,1 +1,17 @@
---
+local cmd = vim.cmd
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-------------------- wbthomason/packer.nvim
+local function packer()
+	cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost ~/.local/share/chezmoi/dot_config/nvim/* :PackerCompile
+  augroup end
+]])
+end
+
+return {
+	packer = packer,
+}
