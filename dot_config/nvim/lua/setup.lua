963cab1160e4da4fa9a371f9246fb46256756008
HighRune <cha.cascio@gmail.com>
2022-04-29 19:43:38 +0200

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -1,1 +1,11 @@
---
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local map = vim.keymap.set
+local call = vim.call
+local api = vim.api
+local silent = { silent = true }
+local expr = { expr = true }
+local remap = { remap = true }
+
+return {}
