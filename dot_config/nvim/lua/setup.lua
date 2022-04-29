45a985f6c110d720df31f9277e13d63b3677e63e
HighRune <cha.cascio@gmail.com>
2022-04-29 19:44:59 +0200

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -4,8 +4,24 @@ local g = vim.g
 local map = vim.keymap.set
 local call = vim.call
 local api = vim.api
-local silent = { silent = true }
-local expr = { expr = true }
-local remap = { remap = true }
 
-return {}
+-------------------- norcalli/nvim-colorizer.lua
+local function colorizer()
+	require("colorizer").setup()
+end
+
+-------------------- ahmedkhalf/project.nvim
+local function project()
+	require("project_nvim").setup()
+end
+
+-------------------- nacro90/numb.nvim
+local function numb()
+	require("numb").setup()
+end
+
+return {
+	colorizer = colorizer,
+	project = project,
+	numb = numb,
+}
