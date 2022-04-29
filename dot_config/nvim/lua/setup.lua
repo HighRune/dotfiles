7707c74eeab52442740daad5f8b27ca14cd3466f
HighRune <cha.cascio@gmail.com>
2022-04-29 19:47:53 +0200

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -20,8 +20,41 @@ local function numb()
 	require("numb").setup()
 end
 
+-------------------- ibhagwan/fzf-lua
+local function fzf()
+	require("fm-nvim").setup({
+		edit_cmd = "edit",
+		ui = {
+			default = "float",
+			float = {
+				border = "none",
+				float_hl = "Normal",
+				border_hl = "FloatBorder",
+				blend = 0,
+				height = 999,
+				width = 999,
+			},
+			split = {
+				direction = "topleft",
+				size = 24,
+			},
+		},
+		cmds = {
+			vifm_cmd = "vifm",
+		},
+		mappings = {
+			-- vert_split = "<C-v>",
+			-- horz_split = "<C-h>",
+			-- tabedit = "<C-t>",
+			-- edit = "<C-e>",
+			-- ESC = "<ESC>",
+		},
+	})
+end
+
 return {
 	colorizer = colorizer,
 	project = project,
 	numb = numb,
+	fzf = fzf,
 }
