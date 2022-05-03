6914612ccc5c1365d10190e7ede56b9780547991
HighRune <cha.cascio@gmail.com>
2022-05-03 19:32:11 +0200

    Update dot_config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -14,7 +14,7 @@ local function core()
 	-- Help
 	cmd("cnoreabbrev <expr> h getcmdtype() == ':' && getcmdline() == 'h' ? 'tab h' : 'h'")
 	cmd("cnoreabbrev <expr> help getcmdtype() == ':' && getcmdline() == 'help' ? 'tab help' : 'help'")
-	cmd("iabbrev co console.log('<Esc>F%s<c-o>:call getchar()<CR>i")
+	cmd("iabbrev co console.log('<Esc>")
 	-- Disable keys
 	map("", "Q", "<nop>")
 	map("", "q", "<nop>")
