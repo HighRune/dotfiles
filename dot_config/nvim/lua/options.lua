e749a2c64ebf92db112c96a65c92ec7180950e32
HighRune <cha.cascio@gmail.com>
2022-05-05 19:04:40 +0200

    Update dot_config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -171,7 +171,7 @@ local function textobjuser()
   	call textobj#user#plugin('contiuous', {
 	\   'continuous': {
 	\     'pattern': '\S\+',
-	\     'select': ['w', 'iw'],
+	\     'select': ['aw', 'iw'],
   \   },
 	\ })
   ]])
