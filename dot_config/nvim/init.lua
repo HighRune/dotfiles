b70d5e692831a3572fb715240be9a639b8e673f1
HighRune <cha.cascio@gmail.com>
2022-05-24 20:02:51 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -220,23 +220,23 @@ return require("packer").startup({
       "RRethy/nvim-base16",
       config = function()
         require('base16-colorscheme').setup({
-          base00= "#161616",
-          base01= "#262626",
-          base02= "#393939",
-          base03= "#525252",
-          base04= "#6F6F6F",
-          base05= "#FAFAFA",
-          base06= "#FAFAFA",
-          base07= "#FFFFFF",
-          base08= "#be95ff",
-          base09= "#3ddbd9",
-          base0A= "#0043ce",
-          base0B= "#33b1ff",
-          base0C= "#ff7eb6",
-          base0D= "#42be65",
-          base0E= "#be95ff",
-          base0F= "#3ddbd9",
-})
+          base00 = "none",
+          base01 = "#262626",
+          base02 = "#393939",
+          base03 = "#525252",
+          base04 = "#6F6F6F",
+          base05 = "#FAFAFA",
+          base06 = "#FAFAFA",
+          base07 = "#FFFFFF",
+          base08 = "#be95ff",
+          base09 = "#3ddbd9",
+          base0A = "#0043ce",
+          base0B = "#33b1ff",
+          base0C = "#ff7eb6",
+          base0D = "#42be65",
+          base0E = "#be95ff",
+          base0F = "#3ddbd9",
+        })
       end
     })
     use({
