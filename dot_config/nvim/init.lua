3e18389a2e12e0526d92c2a2c20bc9102b83c487
HighRune <cha.cascio@gmail.com>
2022-05-24 19:57:32 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,9 +16,9 @@ return require("packer").startup({
     use("kyazdani42/nvim-web-devicons")
     use({
       "folke/tokyonight.nvim",
-      config = function()
-        require("options").tokyonight()
-      end,
+      -- config = function()
+      --   require("options").tokyonight()
+      -- end,
     })
     use({
       "nvim-treesitter/nvim-treesitter",
@@ -215,6 +215,17 @@ return require("packer").startup({
         require("options").submode()
         require("mappings").submode()
       end,
+    })
+    use({
+      "RRethy/nvim-base16",
+      config = function()
+        require('base16-colorscheme').setup({
+    base00 = '#16161D', base01 = '#2c313c', base02 = '#3e4451', base03 = '#6c7891',
+    base04 = '#565c64', base05 = '#abb2bf', base06 = '#9a9bb3', base07 = '#c5c8e6',
+    base08 = '#e06c75', base09 = '#d19a66', base0A = '#e5c07b', base0B = '#98c379',
+    base0C = '#56b6c2', base0D = '#0184bc', base0E = '#c678dd', base0F = '#a06949',
+})
+      end
     })
     use({
       "/home/charles/Documents/dev/plugins/booster.nvim",
