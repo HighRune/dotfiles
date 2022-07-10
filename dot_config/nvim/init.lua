1869d9cbb723ce3d0c5a86519afe31f0a2a758b2
HighRune <cha.cascio@gmail.com>
2022-07-10 22:57:26 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -193,6 +193,10 @@ return require("packer").startup({
       end,
     })
     use({
+      "luukvbaal/stabilize.nvim",
+      config = function() require("stabilize").setup() end
+    })
+    use({
       "chaoren/vim-wordmotion",
       config = function()
         require("mappings").wordmotion()
