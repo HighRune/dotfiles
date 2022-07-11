ee36acaebd75bb69c38d93ba305a58428f9f56bd
HighRune <cha.cascio@gmail.com>
2022-07-12 00:35:42 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -179,10 +179,10 @@ return require("packer").startup({
           'p',
           'g',
         }
-        require('leap').opts.special_keys = {
-          next_match = 'n',
-          prev_match = 'N',
-        }
+        -- require('leap').opts.special_keys = {
+        --   next_match = 'n',
+        --   prev_match = 'N',
+        -- }
       end
     })
     -- use({
