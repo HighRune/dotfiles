3b9dd96b558bb125cdb534c734fabf22a41070b3
HighRune <cha.cascio@gmail.com>
2022-07-25 19:51:28 +0200

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -128,7 +128,30 @@ return require("packer").startup({
     -- })
     use({
       'romgrk/barbar.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons' }
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function()
+        require('bufferline').setup {
+          animation = true,
+          auto_hide = false,
+          tabpages = true,
+          closable = false,
+          clickable = true,
+          icons = true,
+          icon_custom_colors = false,
+          icon_separator_active = '',
+          icon_separator_inactive = '',
+          icon_close_tab = '',
+          icon_close_tab_modified = '●',
+          icon_pinned = '車',
+          insert_at_end = false,
+          insert_at_start = false,
+          maximum_padding = 1,
+          maximum_length = 30,
+          semantic_letters = true,
+          letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+          no_name_title = nil,
+        }
+      end,
     })
     use({
       "kana/vim-arpeggio",
