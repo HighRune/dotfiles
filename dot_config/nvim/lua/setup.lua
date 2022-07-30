d07adb0d924b47a0a675e5dd59290b238096e450
HighRune <cha.cascio@gmail.com>
2022-07-30 23:44:15 +0200

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -152,6 +152,41 @@ local function indentscope()
   })
 end
 
+local function ai()
+  require('mini.ai').setup({
+    {
+      -- Table with textobject id as fields, textobject specification as values.
+      -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+      custom_textobjects = nil,
+
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        -- Main textobject prefixes
+        around = 'a',
+        inside = 'i',
+
+        -- Next/last variants
+        around_next = 'an',
+        inside_next = 'in',
+        around_last = 'al',
+        inside_last = 'il',
+
+        -- Move cursor to corresponding edge of `a` textobject
+        goto_left = 'g[',
+        goto_right = 'g]',
+      },
+
+      -- Number of lines within which textobject is searched
+      n_lines = 50,
+
+      -- How to search for object (first inside current line, then inside
+      -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+      -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+      search_method = 'cover_or_next',
+    }
+  })
+end
+
 local function treesitter()
   require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
@@ -373,5 +408,6 @@ return {
   treesitter = treesitter,
   cybu = cybu,
   base16 = base16,
-  installer = installer
+  installer = installer,
+  ai = ai,
 }
