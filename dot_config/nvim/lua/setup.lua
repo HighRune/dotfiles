263b3e3f263ed08b8dceb353884bacd0939821d7
HighRune <cha.cascio@gmail.com>
2022-09-03 22:27:18 +0200

    Update dot_config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -342,6 +342,54 @@ local function leap()
   }
 end
 
+-------------------- windwp/nvim-autopairs
+local function autopairs()
+  local remap = vim.api.nvim_set_keymap
+  local npairs = require("nvim-autopairs")
+
+  npairs.setup({
+    map_bs = true,
+    map_cr = false,
+    check_ts = true,
+    ignored_next_char = "[%w%.]",
+    -- fast_wrap = {
+    -- 	map = "<C-h>",
+    -- 	chars = { "{", "[", "(", '"', "'" },
+    -- 	pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    -- 	offset = 0,
+    -- 	end_key = "s",
+    -- 	keys = "aoeuhtns",
+    -- 	check_comma = true,
+    -- 	highlight = "Search",
+    -- 	highlight_grey = "Comment",
+    -- },
+  })
+
+  _G.MUtils = {}
+
+  MUtils.CR = function()
+    if fn.pumvisible() ~= 0 then
+      if fn.complete_info({ "selected" }).selected ~= -1 then
+        return npairs.esc("<c-y>")
+      else
+        return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+      end
+    else
+      return npairs.autopairs_cr()
+    end
+  end
+  remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+
+  MUtils.BS = function()
+    if fn.pumvisible() ~= 0 and fn.complete_info({ "mode" }).mode == "eval" then
+      return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+    else
+      return npairs.autopairs_bs()
+    end
+  end
+  remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
+end
+
 return {
   colorizer = colorizer,
   project = project,
@@ -360,4 +408,5 @@ return {
   dial = dial,
   tokyonight = tokyonight,
   highlightedyank = highlightedyank,
+  autopairs = autopairs,
 }
