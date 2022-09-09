099668f06f1f5fe76b3c0cdcf038eec91296fbdc
HighRune <cha.cascio@gmail.com>
2022-09-09 20:40:56 +0200

    Update dot_config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -13,7 +13,6 @@ local expr = { expr = true }
 local remap = { remap = true }
 
 local function core()
-  -- map("n", "<leader>c", ":!chezmoi add %:p<CR>")
   map("n", "<leader>c", ":silent! !chezmoi add %:p<CR>")
   -- command = "silent! !chezmoi add ~/.config/cheatsheet.md",
   -- "silent! !chezmoi add ~/.config/cheatsheet.md",
