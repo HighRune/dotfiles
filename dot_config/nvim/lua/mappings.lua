86becb1988a8c566cee24e7ab36f39795106c8d1
HighRune <cha.cascio@gmail.com>
2022-09-11 00:08:02 +0200

    Update dot_config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -13,7 +13,8 @@ local expr = { expr = true }
 local remap = { remap = true }
 
 local function core()
-  map("n", "<leader>c", ":silent! !chezmoi add %:p &<CR>")
+  map("n", "<leader>c", ":!chezmoi add %:p &<CR>")
+  -- map("n", "<leader>c", ":silent! !chezmoi add %:p &<CR>")
   -- Help
   cmd("cnoreabbrev <expr> h getcmdtype() == ':' && getcmdline() == 'h' ? 'tab h' : 'h'")
   cmd("cnoreabbrev <expr> help getcmdtype() == ':' && getcmdline() == 'help' ? 'tab help' : 'help'")
