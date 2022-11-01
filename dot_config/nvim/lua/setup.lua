523dfc170c1446a454191b169cfd1374f2f75c78
HighRune <cha.cascio@gmail.com>
2022-11-01 11:58:05 +0100

    Update .config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -44,9 +44,20 @@ end
 -------------------- woosaaahh/sj.nvim
 local function sj()
   require("sj").setup({
-    auto_jump = false,
-    use_overlay = false,
-    separator = " ",
+    auto_jump = false, -- if true, automatically jump on the sole match
+    forward_search = true, -- if true, the search will be done from top to bottom
+    highlights_timeout = 0, -- if > 0, wait for 'updatetime' + N ms to clear hightlights (sj.prev_match/sj.next_match)
+    max_pattern_length = 0, -- if > 0, wait for a label after N characters
+    pattern_type = "vim", -- how to interpret the pattern (lua_plain, lua, vim, vim_very_magic)
+    preserve_highlights = true, -- if true, create an autocmd to preserve highlights when switching colorscheme
+    prompt_prefix = "", -- if set, the string will be used as a prefix in the command line
+    relative_labels = false, -- if true, labels are ordered from the cursor position, not from the top of the buffer
+    search_scope = "visible_lines", -- (current_line, visible_lines_above, visible_lines_below, visible_lines, buffer)
+    separator = " ", -- character used to split the user input in <pattern> and <label>
+    update_search_register = false, -- if true, update the search register with the last used pattern
+    use_last_pattern = false, -- if true, reuse the last pattern for next calls
+    use_overlay = false, -- if true, apply an overlay to better identify labels and matches
+    wrap_jumps = o.wrapscan, -- if true, wrap the jumps when focusing previous or next label
     labels = {
       "'", ",", ".", "p", "y", "a", "o", "e", "u", "i", "d", "h", "t",
       "n", "s", "f", "g", "c", "r", "l", ";", "q", "j", "k", "x", "b",
@@ -55,6 +66,19 @@ local function sj()
     highlights = {
       SjLabel = { bg = '#ccff88', fg = 'black' },
       SjMatches = { bg = '#77aaff', fg = 'black' },
+    },
+    keymaps = {
+      cancel = "<Esc>", -- cancel the search
+      validate = "<CR>", -- jump to the focused match
+      prev_match = "<A-,>", -- focus the previous match
+      next_match = "<A-;>", -- focus the next match
+      prev_pattern = "<C-p>", -- select the previous pattern while searching
+      next_pattern = "<C-n>", -- select the next pattern while searching
+      delete_prev_char = "<BS>", -- delete the previous character
+      delete_prev_word = "<C-w>", -- delete the previous word
+      delete_pattern = "<C-u>", -- delete the whole pattern
+      restore_pattern = "<A-BS>", -- restore the pattern to the last version having matches
+      send_to_qflist = "<A-q>", --- send the search results to the quickfix list
     },
   })
 end
