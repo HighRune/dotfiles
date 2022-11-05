ed8f869fb6ce38db9c2331ab33660d772733bc97
HighRune <cha.cascio@gmail.com>
2022-11-05 17:19:11 +0100

    Update .config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -207,7 +207,7 @@ end
 
 -------------------- is0n/fm-nvim
 local function fm()
-  map("n", "<leader>n", ":Vifm<CR>")
+  map("n", "<leader>h", ":Vifm<CR>")
 end
 
 -------------------- monaqa/dial.nvim
