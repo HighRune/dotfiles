ff50c40b7b3a2583caeedb6520e7f6bfc1590d5a
HighRune <cha.cascio@gmail.com>
2022-11-12 10:52:23 +0100

    Update .config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -272,7 +272,8 @@ local function ai()
       goto_right = '',
     },
     n_lines = 50,
-    search_method = 'cover_or_nearest',
+    search_method = 'cover_or_next',
+    -- search_method = 'cover_or_nearest',
   })
 end
 
