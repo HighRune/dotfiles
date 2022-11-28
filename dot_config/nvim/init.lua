537eb038ac39fab567a8f399b9d2568a6db38d54
HighRune <cha.cascio@gmail.com>
2022-11-28 21:56:28 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -42,8 +42,8 @@ return require("packer").startup({
     use({
       "mfussenegger/nvim-dap",
       config = function()
-        -- require('mappings').dap()
-        -- require('config.dap')
+        require('mappings').dap()
+        require('config.dap')
       end
     })
     -- use({
