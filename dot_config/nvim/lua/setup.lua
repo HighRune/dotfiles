b4cb38be2984e8eea81bd6b4cb4c865ca3892e06
HighRune <cha.cascio@gmail.com>
2022-12-17 22:07:13 +0100

    Update .config/nvim/lua/setup.lua

--- dot_config/nvim/lua/setup.lua
+++ dot_config/nvim/lua/setup.lua
@@ -185,6 +185,39 @@ local function lightbulb()
   fn.sign_define('LightBulbSign', { text = "⚡" })
 end
 
+-------------------- Dkendal/nvim-treeclimber
+local function treeclimber()
+  local tc = require('nvim-treeclimber')
+
+  local color = require("nvim-treeclimber.hi")
+  local bg = color.bg_hsluv("Normal")
+  local fg = color.fg_hsluv("Normal")
+  local dim = bg.mix(fg, 20)
+
+  vim.api.nvim_set_hl(0, "TreeClimberHighlight", { background = dim.hex })
+  vim.api.nvim_set_hl(0, "TreeClimberSiblingBoundary", { background = color.terminal_color_5.hex })
+  vim.api.nvim_set_hl(0, "TreeClimberSibling", { background = color.terminal_color_5.mix(bg, 40).hex, bold = true })
+  vim.api.nvim_set_hl(0, "TreeClimberParent", { background = bg.mix(fg, 2).hex })
+  vim.api.nvim_set_hl(0, "TreeClimberParentStart", { background = color.terminal_color_4.mix(bg, 10).hex, bold = true })
+
+  map("n", "<leader>k", tc.show_control_flow, {})
+  map({ "x", "o" }, "i.", tc.select_current_node, { desc = "select current node" })
+  map({ "x", "o" }, "a.", tc.select_expand, { desc = "select parent node" })
+  map({ "n", "x", "o" }, "<M-e>", tc.select_forward_end,
+    { desc = "select and move to the end of the node, or the end of the next node" })
+  map({ "n", "x", "o" }, "<M-b>", tc.select_backward,
+    { desc = "select and move to the begining of the node, or the beginning of the next node" })
+  map({ "n", "x", "o" }, "<M-[>", tc.select_siblings_backward, {})
+  map({ "n", "x", "o" }, "<M-]>", tc.select_siblings_forward, {})
+  map({ "n", "x", "o" }, "<M-g>", tc.select_top_level, { desc = "select the top level node from the current position" })
+  map({ "n", "x", "o" }, "<M-h>", tc.select_backward, { desc = "select previous node" })
+  map({ "n", "x", "o" }, "<M-j>", tc.select_shrink, { desc = "select child node" })
+  map({ "n", "x", "o" }, "<M-k>", tc.select_expand, { desc = "select parent node" })
+  map({ "n", "x", "o" }, "<M-l>", tc.select_forward, { desc = "select the next node" })
+  map({ "n", "x", "o" }, "<M-L>", tc.select_grow_forward, { desc = "Add the next node to the selection" })
+  map({ "n", "x", "o" }, "<M-H>", tc.select_grow_backward, { desc = "Add the next node to the selection" })
+end
+
 -------------------- ibhagwan/fzf-lua
 local function fzf()
   local actions = require("fzf-lua.actions")
@@ -701,4 +734,5 @@ return {
   sj = sj,
   highlightedyank = highlightedyank,
   matcharea = matcharea,
+  treeclimber = treeclimber,
 }
