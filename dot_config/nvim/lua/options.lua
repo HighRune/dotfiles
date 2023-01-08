0ee4c66e931cc6d11b741551de414356b92765e4
HighRune <cha.cascio@gmail.com>
2023-01-08 23:28:55 +0100

    Update .config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -61,8 +61,29 @@ local function core()
   o.foldexpr = "nvim_treesitter#foldexpr()"
 end
 
+-------------------- andymass/vim-matchup
+local function matchup()
+  g.loaded_matchit = 1
+  g.matchup_matchparen_nomode = "i"
+  g.matchup_mappings_enabled = 0
+  g.matchup_matchparen_pumvisible = 0
+  g.matchup_matchparen_deferred = 1
+  g.matchup_matchparen_deferred_show_delay = 50
+  g.matchup_matchparen_deferred_hide_delay = 50
+  g.matchup_motion_override_Npercent = 0
+  g.matchup_surround_enabled = 1
+  g.matchup_motion_enabled = 1
+  g.matchup_text_obj_enabled = 1
+  g.matchup_transmute_enabled = 1
+  g.matchup_matchparen_enabled = 1
+  g.matchup_override_vimtex = 1
+  g.matchup_matchparen_hi_background = 1
+  g.matchup_matchparen_offscreen = { method = "popup", fullwidth = 1, }
+end
+
 return {
   core = core,
+  matchup = matchup,
 }
 
 -- local function textobjuser()
